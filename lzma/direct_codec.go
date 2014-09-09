b9b22edeb5879af120792bb3bfd3278db2ba295a
Ulrich Kunitz <uli-go@users.noreply.github.com>
2014-09-09 21:22:10 +0200

    lzma: updates to the direct codec
    The encoder and decoder are now bytes instead of structs. The new
    functions return the actual value. Documentation for all functions
    added.

--- lzma/direct_codec.go
+++ lzma/direct_codec.go
@@ -2,19 +2,29 @@ package lzma
 
 import "fmt"
 
-type directEncoder struct {
-	bits byte
-}
+// directEncoder supports the "direct" encoding of values with a fixed number of
+// bits. Direct encoding means the use of the range encoder with probability
+// 1/2 for zero and one bits. The supported range of byte values is [1,32].
+type directEncoder byte
 
-func newDirectEncoder(bits byte) *directEncoder {
+// newDirectEncoder returns a directEncoder. The function panics if the bits
+// argument is outside of the range [1,32].
+func newDirectEncoder(bits byte) directEncoder {
 	if !(1 <= bits && bits <= 32) {
 		panic(fmt.Errorf("bits=%d out of range", bits))
 	}
-	return &directEncoder{bits}
+	return directEncoder(bits)
 }
 
-func (de *directEncoder) Encode(v uint32, e *rangeEncoder) error {
-	for i := int(de.bits) - 1; i >= 0; i-- {
+// Returns the number of bits for which encoding is supported.
+func (de directEncoder) Bits() int {
+	return int(de)
+}
+
+// Encode uses the range encoder to encode a value with the fixed number of
+// bits. The most-significant bit is encoded first.
+func (de directEncoder) Encode(v uint32, e *rangeEncoder) error {
+	for i := int(de) - 1; i >= 0; i-- {
 		if err := e.DirectEncodeBit(v >> uint(i)); err != nil {
 			return err
 		}
@@ -22,19 +32,28 @@ func (de *directEncoder) Encode(v uint32, e *rangeEncoder) error {
 	return nil
 }
 
-type directDecoder struct {
-	bits byte
-}
+// directDecoder support the "direct" decoding of values with a fixed number of
+// bits.
+type directDecoder byte
 
-func newDirectDecoder(bits byte) *directDecoder {
+// newDirectDecoder returns a directDecoder. The function panics if the bits
+// argument is outside the range [1,32.].
+func newDirectDecoder(bits byte) directDecoder {
 	if !(1 <= bits && bits <= 32) {
 		panic(fmt.Errorf("bits=%d out of range", bits))
 	}
-	return &directDecoder{bits}
+	return directDecoder(bits)
 }
 
-func (dd *directDecoder) Decode(d *rangeDecoder) (v uint32, err error) {
-	for i := int(dd.bits) - 1; i >= 0; i-- {
+// Bits returns the number of bits for which values can be decoded.
+func (dd directDecoder) Bits() int {
+	return int(dd)
+}
+
+// Decode uses the range decoder to decode a value with the given number of
+// given bits. The most-significant bit is decoded first.
+func (dd directDecoder) Decode(d *rangeDecoder) (v uint32, err error) {
+	for i := int(dd) - 1; i >= 0; i-- {
 		x, err := d.DirectDecodeBit()
 		if err != nil {
 			return 0, err
