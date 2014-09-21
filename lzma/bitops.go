41ff8697909409b3146f06eb1f293de48a5a70ef
Ulrich Kunitz <uli-go@users.noreply.github.com>
2014-09-21 14:31:54 +0200

    lzma: added documentation to bitops.go

--- lzma/bitops.go
+++ lzma/bitops.go
@@ -1,13 +1,17 @@
 package lzma
 
+// ntz32Const is used by ntz32 and nlz32.
 const ntz32Const = 0x04d7651f
 
+// Helper table for de Bruijn algorithm. See Henry S. Warren, Jr. "Hacker's
+// Delight" section 5-1 figure 5-26.
 var ntz32Table = [32]int8{
 	0, 1, 2, 24, 3, 19, 6, 25,
 	22, 4, 20, 10, 16, 7, 12, 26,
 	31, 23, 18, 5, 21, 9, 15, 11,
 	30, 17, 8, 14, 29, 13, 28, 27}
 
+// ntz32 computes the number of trailing zeros for an unsigned 32-bit integer.
 func ntz32(x uint32) int {
 	if x == 0 {
 		return 32
@@ -16,6 +20,7 @@ func ntz32(x uint32) int {
 	return int(ntz32Table[x>>27])
 }
 
+// nlz32 computes the number of leading zeros for an unsigned 32-bit integer.
 func nlz32(x uint32) int {
 	// Smear left most bit to the right
 	x |= x >> 1
