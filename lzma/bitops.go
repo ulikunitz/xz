d34674d4e28f7ad0994f5afe62f544c4886c30c1
Ulrich Kunitz <uli-go@users.noreply.github.com>
2014-09-21 13:09:41 +0200

    lzma: added ntz32 to bitops.go
    The number of trailing zero function is actually needed by the distance
    encoder.

--- lzma/bitops.go
+++ lzma/bitops.go
@@ -1,5 +1,7 @@
 package lzma
 
+const ntz32Const = 0x04d7651f
+
 var ntz32Table = [32]int8{
 	0, 1, 2, 24, 3, 19, 6, 25,
 	22, 4, 20, 10, 16, 7, 12, 26,
@@ -10,6 +12,22 @@ func ntz32(x uint32) int {
 	if x == 0 {
 		return 32
 	}
-	x = (x & -x) * 0x04d7651f
+	x = (x & -x) * ntz32Const
 	return int(ntz32Table[x>>27])
+}
+
+func nlz32(x uint32) int {
+	// Smear left most bit to the right
+	x |= x >> 1
+	x |= x >> 2
+	x |= x >> 4
+	x |= x >> 8
+	x |= x >> 16
+	// Use ntz mechanism to calculate nlz.
+	x += 1
+	if x == 0 {
+		return 0
+	}
+	x *= ntz32Const
+	return 32 - int(ntz32Table[x>>27])
 }
--- lzma/bitops_test.go
+++ lzma/bitops_test.go
@@ -16,3 +16,18 @@ func TestNtz32(t *testing.T) {
 		}
 	}
 }
+
+func TestNlz32(t *testing.T) {
+	for i := 0; i < 32; i++ {
+		x := uint32(0x80000000) >> uint(i)
+		k := nlz32(x)
+		if k != i {
+			t.Errorf("nlz32(%#08x) = %d; want %d", x, k, i)
+		}
+		x = uint32(0xefffffef) >> uint(i)
+		k = nlz32(x)
+		if k != i {
+			t.Errorf("nlz32(%#08x) = %d; want %d", x, k, i)
+		}
+	}
+}
