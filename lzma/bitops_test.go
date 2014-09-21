567c6e89a63f7d4e4908d9777b956ad47d2e3499
Ulrich Kunitz <uli-go@users.noreply.github.com>
2014-09-21 13:16:02 +0200

    lzma: ensure that all potential cases are tested
    The case for x=0 has not been tested for nlz32 and ntz32.

--- lzma/bitops_test.go
+++ lzma/bitops_test.go
@@ -3,7 +3,7 @@ package lzma
 import "testing"
 
 func TestNtz32(t *testing.T) {
-	for i := 0; i < 31; i++ {
+	for i := 0; i <= 32; i++ {
 		x := uint32(1) << uint(i)
 		k := ntz32(x)
 		if k != i {
@@ -18,7 +18,7 @@ func TestNtz32(t *testing.T) {
 }
 
 func TestNlz32(t *testing.T) {
-	for i := 0; i < 32; i++ {
+	for i := 0; i <= 32; i++ {
 		x := uint32(0x80000000) >> uint(i)
 		k := nlz32(x)
 		if k != i {
