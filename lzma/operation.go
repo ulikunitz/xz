56cde62d797283704370c5c67b755ea5961e8510
Ulrich Kunitz <uli-go@users.noreply.github.com>
2014-10-26 15:10:39 +0100

    lzma: adds the operation interface

---
+++ lzma/operation.go
@@ -0,0 +1,51 @@
+package lzma
+
+import "fmt"
+
+// operation represents an operation on the dictionary during encoding or
+// decoding.
+type operation interface {
+	applyDecoderDict(d *decoderDict) error
+	Len() int
+}
+
+// rep represents a repetition at the given distance and the given length
+type rep struct {
+	length   int
+	distance int
+}
+
+// applyDecoderDict applies the repetition on the decoder dictionary.
+func (r rep) applyDecoderDict(d *decoderDict) error {
+	return d.CopyMatch(r.distance, r.length)
+}
+
+// Len return the length of the repetition.
+func (r rep) Len() int {
+	return r.length
+}
+
+// String returns a string representation for the repetition.
+func (r rep) String() string {
+	return fmt.Sprintf("rep(%d,%d)", r.distance, r.length)
+}
+
+// lit represents a single byte literal.
+type lit struct {
+	b byte
+}
+
+// applyDecoder appends the literal to the decoder dictionary.
+func (l lit) applyDecoder(d *decoderDict) error {
+	return d.AddByte(l.b)
+}
+
+// Len returns 1 for the single byte literal.
+func (l lit) Len() int {
+	return 1
+}
+
+// String returns a string representation for the literal.
+func (l lit) String() string {
+	return fmt.Sprintf("lit(%02x)", l.b)
+}
