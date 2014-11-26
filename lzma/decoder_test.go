f161a4312dc29ddccd020fe362a1a01db97a84f7
Ulrich Kunitz <uli-go@users.noreply.github.com>
2014-11-26 17:26:33 +0100

    lzma: all test files from the LZMA spec successfully decoded

--- lzma/decoder_test.go
+++ lzma/decoder_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -58,5 +59,59 @@ func TestDecoderSimple(t *testing.T) {
 	}
 	if !bytes.Equal(orig, decompressed) {
 		t.Fatalf("decompressed file differs from original")
+	}
+}
+
+func TestDecoderAll(t *testing.T) {
+	dirname := "examples"
+	dir, err := os.Open(dirname)
+	if err != nil {
+		t.Fatalf("Open: %s", err)
+	}
+	defer dir.Close()
+	all, err := dir.Readdirnames(0)
+	if err != nil {
+		t.Fatalf("Readdirnames: %s", err)
+	}
+	files := make([]string, 0, len(all))
+	for _, fn := range all {
+		match, err := filepath.Match("a*.lzma", fn)
+		if err != nil {
+			t.Fatalf("Match: %s", err)
+		}
+		if match {
+			files = append(files, fn)
+		}
+	}
+	t.Log("files:", files)
+	origFn := filepath.Join(dirname, "a.txt")
+	orig, err := ioutil.ReadFile(origFn)
+	if err != nil {
+		t.Fatalf("ReadFile(\"%s\"): %s", origFn, err)
+	}
+	for _, fn := range files {
+		pn := filepath.Join(dirname, fn)
+		f, err := os.Open(pn)
+		if err != nil {
+			t.Fatalf("Open(\"%s\"): %s", pn, err)
+		}
+		defer f.Close()
+		t.Logf("file %s opened", fn)
+		d, err := NewDecoder(f)
+		if err != nil {
+			t.Fatalf("NewDecoder: %s", err)
+		}
+		decompressed, err := ioutil.ReadAll(d)
+		if err != nil {
+			t.Fatalf("ReadAll: %s", err)
+		}
+		t.Logf("uncompressed:\n%s", decompressed)
+		if len(orig) != len(decompressed) {
+			t.Fatalf("length decompressed is %d; want %d",
+				len(decompressed), len(orig))
+		}
+		if !bytes.Equal(orig, decompressed) {
+			t.Fatalf("decompressed file differs from original")
+		}
 	}
 }
