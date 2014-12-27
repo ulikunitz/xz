ebe344b8931ea74ef3e012fd7ce8771064ef057f
Ulrich Kunitz <uli-go@users.noreply.github.com>
2014-12-27 12:23:47 +0100

    lzma: more work on encoder dictionary

--- lzma/encoder_dict.go
+++ lzma/encoder_dict.go
@@ -37,9 +37,78 @@ func newEncoderDict(bufferLen int, historyLen int) (p *encoderDict, err error) {
 			"LZMA dictionary size overflows integer range")
 	}
 	p = &encoderDict{
-		data: make([]byte, 0, z),
+		data: make([]byte, z),
 		h:    historyLen,
 		b:    bufferLen,
 	}
 	return p, nil
+}
+
+// buffered returns the number of buffered bytes that have not been consumed so
+// far
+func (d *encoderDict) buffered() int {
+	delta := d.w - d.c
+	if delta < 0 {
+		delta += len(d.data)
+	}
+	return delta
+}
+
+// writable computes the maximum number of bytes supported to be written
+func (d *encoderDict) writable() int {
+	if d.eof {
+		return 0
+	}
+	n := d.b - d.buffered()
+	if n < 0 {
+		panic("more data buffered than buffer length")
+	}
+	return n
+}
+
+// Write writes data into the buffer. If not all bytes can be written the
+// number of bytes written and the error code errOverflow is returned.
+func (d *encoderDict) Write(p []byte) (n int, err error) {
+	n = d.writable()
+	if n < len(p) {
+		err = errOverflow
+	} else {
+		n = len(p)
+	}
+	p = p[:n]
+	if d.w >= d.c {
+		m := len(d.data) - d.w
+		if m > len(p) {
+			m = len(p)
+		}
+		copy(d.data[d.w:], p[:m])
+		d.w += m
+		if d.w == len(d.data) {
+			d.w = 0
+		}
+		p = p[m:]
+	}
+	if len(p) > 0 {
+		m := copy(d.data[d.w:], p)
+		d.w += m
+	}
+	return n, errOverflow
+}
+
+// move advances the c pointer by n bytes. The argument n must be nonnegative
+// and is not allowed to become greater than the bytes actually buffered in the
+// encoder dictionary.
+func (d *encoderDict) move(n int) (err error) {
+	if n < 0 {
+		return newError("n must be nonnegative")
+	}
+	if n > d.buffered() {
+		return newError("can't skip more bytes then buffered")
+	}
+	d.c += n - len(d.data)
+	if d.c < 0 {
+		d.c += len(d.data)
+	}
+	d.total += int64(n)
+	return nil
 }
