e7d1128d8bbd6b6115bc8d717cdfdf3a84bd0271
Ulrich Kunitz <uli-go@users.noreply.github.com>
2014-12-29 22:17:19 +0100

    lzma: added documentation comments to writer.go

--- lzma/writer.go
+++ lzma/writer.go
@@ -2,21 +2,30 @@ package lzma
 
 import "io"
 
-// Writer supports the LZMA compression of a file. Note that the LZMA encoder
-// doesn't support flushing because of the arithmetic coding included.
+// Writer supports the LZMA compression of a file. It cannot support flushing
+// because of the arithmethic coder.
 type Writer struct {
 	unpackLen  uint64
 	writtenLen uint64
 }
 
+// NewWriter creates a new LZMA writer using the given properties. It doesn't
+// provide an unpack length and creates an explicit end of stream. The classic
+// LZMA header will be created.
 func NewWriter(w io.Writer, p *Properties) (*Writer, error) {
 	return NewWriterLenEOS(w, p, NoUnpackLen, true)
 }
 
+// NewWriterLen creates a new LZMA writer and a predefined length. There will
+// be no end-of-stream marker created unless NoUnpackLen is used as length.
 func NewWriterLen(w io.Writer, p *Properties, length uint64) (*Writer, error) {
 	return NewWriterLenEOS(w, p, length, false)
 }
 
+// NewWriterLenEOS creates a new LZMA writer. A predefinied length can be
+// provided and the writing of an end-of-stream marker can be controlled. If
+// the argument NoUnpackLen will be provided for the lenght a end-of-stream
+// marker will be written regardless of the eos parameter.
 func NewWriterLenEOS(w io.Writer, p *Properties, length uint64, eos bool) (*Writer, error) {
 	if length == NoUnpackLen {
 		eos = true
@@ -33,13 +42,3 @@ func (l *Writer) Write(p []byte) (int, error) {
 func (l *Writer) Close() error {
 	panic("TODO")
 }
-
-/*
-
-# Ideas
-
-We need an encoder dictionary in which data can be written. For the encoder the
-dictionary should be not be bigger than the history size. We continue to use a
-ring buffer.
-
-*/
