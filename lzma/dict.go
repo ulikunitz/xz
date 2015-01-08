32f8c60b0d3e44a80c0de112bb1e442a118f156e
Ulrich Kunitz <uli-go@users.noreply.github.com>
2015-01-08 09:19:29 +0100

    lzma: added documentation to the readerDict type

--- lzma/dict.go
+++ lzma/dict.go
@@ -1,14 +1,17 @@
 package lzma
 
-var (
-	errDist = newError("distance out of range")
-)
+// errDist indicates that the distance is out of range.
+var errDist = newError("distance out of range")
 
+// readerDict represents the dictionary for reading. It is a ring buffer using
+// the end field as head for the dictionary.
 type readerDict struct {
 	buffer
 	bufferLen int
 }
 
+// newReaderDict creates a new reader dictionary. The capacity of the ring
+// buffer will be the maximum of historyLen and bufferLen.
 func newReaderDict(historyLen, bufferLen int) (rd *readerDict, err error) {
 	if !(1 <= historyLen && int64(historyLen) < MaxDictLen) {
 		return nil, newError("historyLen out of range")
@@ -25,10 +28,13 @@ func newReaderDict(historyLen, bufferLen int) (rd *readerDict, err error) {
 	return
 }
 
+// Offset returns the offset of the dictionary head.
 func (rd *readerDict) Offset() int64 {
 	return rd.end
 }
 
+// WriteRep writes a repetition with the given distance. While distance is
+// given here as int64 the actual limit is the maximum of the int type.
 func (rd *readerDict) WriteRep(dist int64, n int) (written int, err error) {
 	if !(1 <= dist && dist <= int64(rd.Len())) {
 		return 0, errDist
@@ -36,6 +42,7 @@ func (rd *readerDict) WriteRep(dist int64, n int) (written int, err error) {
 	return rd.WriteRepOff(n, rd.end-dist)
 }
 
+// Byte returns a byte at the given distance.
 func (rd *readerDict) Byte(dist int) byte {
 	c, _ := rd.ReadByteAt(rd.end - int64(dist))
 	return c
