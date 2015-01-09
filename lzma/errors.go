099e79b4ad49134d652be06648d7a4be7a881e13
Ulrich Kunitz <uli-go@users.noreply.github.com>
2015-01-09 11:52:32 +0100

    lzma: hide Error from the public interface

--- lzma/errors.go
+++ lzma/errors.go
@@ -1,21 +1,17 @@
 package lzma
 
-// Error represents an LZMA-specific error. It allows the testing against LZMA
-// errors.
-//
-//  if _, ok := err.(lzma.Error); ok {
-//  	fmt.Println("lzma error %s", err)
-//  }
-type Error struct {
+// lerror represents an LZMA-specific error. It currently adds the prefix
+// "lzma -" to all errors created in the package.
+type lerror struct {
 	Msg string
 }
 
 // Error returns the error message with the prefix "lzma - ".
-func (e Error) Error() string {
+func (e lerror) Error() string {
 	return "lzma - " + e.Msg
 }
 
 // newError creates a new lzma error with the given message.
 func newError(msg string) error {
-	return Error{msg}
+	return lerror{msg}
 }
