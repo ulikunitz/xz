b82990e5f5138294a780c491b5a396232298e4cd
Ulrich Kunitz <uli-go@users.noreply.github.com>
2015-01-11 23:00:24 +0100

    xz: package documentation links back to README.md for information

--- doc.go
+++ doc.go
@@ -1,6 +1,7 @@
 // Package xz allows the compression and decompression of xz files.
 //
 // There is no functionality here, because I'm currently working on the
-// components required to support parsing the xz format. Check TODO.md for the
-// actual progress.
+// components required to support parsing the xz format.
+// Check https://github.com/uli-go/xz/blob/master/README.md for the
+// current status.
 package xz
