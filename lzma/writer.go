b8712e6df14c333db21f31cc312ba4bd83ce3a5a
Ulrich Kunitz <uli-go@users.noreply.github.com>
2015-02-26 22:33:55 +0100

    lzma: removed the opWriter type
    As the opReader the opWriter is an unnecessary abstraction. Therefore I
    removed it from the code.

--- lzma/writer.go
+++ lzma/writer.go
@@ -7,8 +7,9 @@ import "io"
 // Using an arithmetic coder it cannot support flushing. A writer must be
 // closed.
 type Writer struct {
+	opCodec
 	w      io.Writer
-	ow     *opWriter
+	re     *rangeEncoder
 	params Parameters
 	dict   *writerDict
 	// hash table for four-byte sequences
@@ -50,10 +51,8 @@ func NewWriterP(w io.Writer, p Parameters) (*Writer, error) {
 	if err != nil {
 		return nil, err
 	}
-	lw.ow, err = newOpWriter(w, &p, lw.dict)
-	if err != nil {
-		return nil, err
-	}
+	lw.re = newRangeEncoder(w)
+	lw.opCodec.init(p.Properties(), lw.dict)
 	lw.t4, err = newHashTable(int(p.DictSize), 4)
 	if err != nil {
 		return nil, err
@@ -102,11 +101,11 @@ func (lw *Writer) Close() error {
 		return err
 	}
 	if lw.params.EOS {
-		if err = lw.ow.writeEOS(); err != nil {
+		if err = lw.writeEOS(); err != nil {
 			return err
 		}
 	}
-	if err = lw.ow.Close(); err != nil {
+	if err = lw.re.Close(); err != nil {
 		return err
 	}
 	return lw.dict.Close()
@@ -137,7 +136,7 @@ func (lw *Writer) potentialOffsets(p []byte) []int64 {
 	}
 	for i := 3; i >= 0; i-- {
 		// distances from the repetition for length less than 4
-		dist := int64(lw.ow.rep[i]) + minDistance
+		dist := int64(lw.rep[i]) + minDistance
 		off := head - dist
 		if start <= off {
 			offs = append(offs, off)
@@ -168,7 +167,7 @@ func (lw *Writer) bestMatch(offsets []int64) (m match, err error) {
 		return
 	}
 	if length == 1 {
-		dist := int64(lw.ow.rep[0]) + minDistance
+		dist := int64(lw.rep[0]) + minDistance
 		offRep0 := head - dist
 		if off != offRep0 {
 			err = errNoMatch
@@ -228,7 +227,7 @@ func (lw *Writer) process(flags int) error {
 			debug.Printf("findOp error %s\n", err)
 			return err
 		}
-		if err = lw.ow.WriteOp(op); err != nil {
+		if err = lw.writeOp(op); err != nil {
 			return err
 		}
 		debug.Printf("op %s", op)
@@ -237,4 +236,119 @@ func (lw *Writer) process(flags int) error {
 		}
 	}
 	return nil
+}
+
+// writeLiteral writes a literal into the operation stream
+func (lw *Writer) writeLiteral(l lit) error {
+	var err error
+	state, state2, _ := lw.states()
+	if err = lw.isMatch[state2].Encode(lw.re, 0); err != nil {
+		return err
+	}
+	litState := lw.litState()
+	match := lw.dict.Byte(int(lw.rep[0]) + 1)
+	err = lw.litCodec.Encode(lw.re, l.b, state, match, litState)
+	if err != nil {
+		return err
+	}
+	lw.updateStateLiteral()
+	return nil
+}
+
+// writeEOS writes the explicit EOS marker
+func (lw *Writer) writeEOS() error {
+	return lw.writeMatch(match{distance: maxDistance, length: minLength})
+}
+
+func iverson(ok bool) uint32 {
+	if ok {
+		return 1
+	}
+	return 0
+}
+
+// writeRep writes a repetition operation into the operation stream
+func (lw *Writer) writeMatch(m match) error {
+	var err error
+	if !(minDistance <= m.distance && m.distance <= maxDistance) {
+		return newError("distance out of range")
+	}
+	dist := uint32(m.distance - minDistance)
+	if !(minLength <= m.length && m.length <= maxLength) &&
+		!(dist == lw.rep[0] && m.length == 1) {
+		return newError("length out of range")
+	}
+	state, state2, posState := lw.states()
+	if err = lw.isMatch[state2].Encode(lw.re, 1); err != nil {
+		return err
+	}
+	var g int
+	for g = 0; g < 4; g++ {
+		if lw.rep[g] == dist {
+			break
+		}
+	}
+	b := iverson(g < 4)
+	if err = lw.isRep[state].Encode(lw.re, b); err != nil {
+		return err
+	}
+	n := uint32(m.length - minLength)
+	if b == 0 {
+		// simple match
+		lw.rep[3], lw.rep[2], lw.rep[1], lw.rep[0] = lw.rep[2],
+			lw.rep[1], lw.rep[0], dist
+		lw.updateStateMatch()
+		if err = lw.lenCodec.Encode(lw.re, n, posState); err != nil {
+			return err
+		}
+		return lw.distCodec.Encode(lw.re, dist, n)
+	}
+	b = iverson(g != 0)
+	if err = lw.isRepG0[state].Encode(lw.re, b); err != nil {
+		return err
+	}
+	if b == 0 {
+		// g == 0
+		b = iverson(m.length != 1)
+		if err = lw.isRepG0Long[state2].Encode(lw.re, b); err != nil {
+			return err
+		}
+		if b == 0 {
+			lw.updateStateShortRep()
+			return nil
+		}
+	} else {
+		// g in {1,2,3}
+		b = iverson(g != 1)
+		if err = lw.isRepG1[state].Encode(lw.re, b); err != nil {
+			return err
+		}
+		if b == 1 {
+			// g in {2,3}
+			b = iverson(g != 2)
+			err = lw.isRepG2[state].Encode(lw.re, b)
+			if err != nil {
+				return err
+			}
+			if b == 1 {
+				lw.rep[3] = lw.rep[2]
+			}
+			lw.rep[2] = lw.rep[1]
+		}
+		lw.rep[1] = lw.rep[0]
+		lw.rep[0] = dist
+	}
+	lw.updateStateRep()
+	return lw.repLenCodec.Encode(lw.re, n, posState)
+}
+
+// writeOp writes an operation value into the stream.
+func (lw *Writer) writeOp(op operation) error {
+	switch x := op.(type) {
+	case match:
+		return lw.writeMatch(x)
+	case lit:
+		return lw.writeLiteral(x)
+	}
+	panic("unknown operation type")
 }
