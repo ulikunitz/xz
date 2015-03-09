89a71a3e7d47934c6abf4e654960f7eb99795e5f
Ulrich Kunitz <uli-go@users.noreply.github.com>
2015-03-10 00:33:19 +0100

    lzma2: added new package to handle the LZMA2 format

---
+++ lzma2/doc.go
@@ -0,0 +1,5 @@
+// Package lzma2 supports reading and writing of the LZMA2 compression format.
+//
+// The LZMA2 format is an improvement of the classic LZMA format. It supports
+// flushing, concurrent compression and uncompressed frames.
+package lzma2
