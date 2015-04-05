bd4555bf222ac9690ad7f759475b69d3a958e0b9
Ulrich Kunitz <uli-go@users.noreply.github.com>
2015-04-05 10:01:26 +0200

    lzma2: initial code for the classic reader

--- lzma2/doc.go
+++ lzma2/doc.go
@@ -1,5 +1,7 @@
-// Package lzma2 supports reading and writing of the LZMA2 compression format.
+// Package lzma2 supports reading and writing of the LZMA2 and the LZMA format.
 //
 // The LZMA2 format is an improvement of the classic LZMA format. It supports
 // flushing, concurrent compression and uncompressed frames.
+//
+// For decoding and encoding of the LZMA format use the ClassicReader.
 package lzma2
