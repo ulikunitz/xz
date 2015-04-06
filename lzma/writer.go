1f5a08cad5201069273135e40361da5ae2f11dab
Ulrich Kunitz <uli-go@users.noreply.github.com>
2015-04-06 20:35:46 +0200

    lzma: added documentation comments to Writer

--- lzma/writer.go
+++ lzma/writer.go
@@ -6,15 +6,33 @@ import (
 	"github.com/uli-go/xz/lzbase"
 )
 
+// Writer supports the LZMA compression of a file.
+//
+// Using an arithmetic coder it cannot support flushing. A writer must be
+// closed.
 type Writer struct {
 	lzbase.Writer
 	params *Parameters
 }
 
+// NewWriter creates a new writer. It writes the LZMA header. It will use the
+// Default Parameters.
+//
+// Don't forget to call Close() for the writer after all data has been written.
+//
+// For high performance use a buffered writer. But be aware that Close will not
+// flush it.
 func NewWriter(w io.Writer) (lw *Writer, err error) {
 	return NewWriterP(w, Default)
 }
 
+// NewWriterP creates a new writer with the given Parameters. It writes the
+// LZMA header.
+//
+// Don't forget to call Close() for the writer after all data has been written.
+//
+// For high performance use a buffered writer. But be aware that Close will not
+// flush it.
 func NewWriterP(w io.Writer, p Parameters) (lw *Writer, err error) {
 	if w == nil {
 		return nil, newError("writer argument w is nil")
