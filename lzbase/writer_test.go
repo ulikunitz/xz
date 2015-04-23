8c65318eb80351af9e1c60d38068cd7883ec1aa7
Ulrich Kunitz <uli-go@users.noreply.github.com>
2015-04-24 00:18:36 +0200

    lzbase: provided LimitedReader
    The LimitedReader provides a LimitedReader for the lzbase.Reader. It
    confirms the EOF condition when the limit is reached by checking that
    the reader dictionary has been completely read and the range decoder is
    drained.
    
    A new test function has been added to writer_test.go that uses the
    LimitedReader.

--- lzbase/writer_test.go
+++ lzbase/writer_test.go
@@ -6,11 +6,12 @@ import (
 	"testing"
 )
 
-func TestWriterCycle(t *testing.T) {
-	const msg = `Das ist ein Test!
+const msg = `Das ist ein Test!
 This is a test!
 Это тест.
 `
+
+func TestWriterCycle(t *testing.T) {
 	var buf bytes.Buffer
 	props, err := NewProperties(3, 0, 2)
 	if err != nil {
@@ -56,6 +57,61 @@ This is a test!
 		t.Fatalf("r.Read returned %q; want %q", q, msg)
 	}
 	n, err = r.Read(q)
+	if err != io.EOF {
+		t.Fatalf("r.Read returned %v; want %v", err, io.EOF)
+	}
+	if n != 0 {
+		t.Fatalf("r.Read returned %d bytes; want %d", n, 0)
+	}
+}
+
+func TestWriterCycleLimited(t *testing.T) {
+	var buf bytes.Buffer
+	props, err := NewProperties(3, 0, 2)
+	if err != nil {
+		t.Fatalf("NewProperties: error %s", err)
+	}
+	const dictSize = 4096
+	wd, err := NewWriterDict(dictSize, dictSize)
+	if err != nil {
+		t.Fatalf("NewWriterDict: error %v", err)
+	}
+	wstate := NewWriterState(props, wd)
+	w := NewWriter(&buf, wstate, true)
+	p := []byte(msg)
+	n, err := w.Write(p)
+	if err != nil {
+		t.Fatalf("w.Write error %s", err)
+	}
+	if n != len(p) {
+		t.Fatalf("w.Write wrote %d bytes; want %d", n, len(p))
+	}
+	if err = w.Close(); err != nil {
+		t.Fatalf("w.Close error %s", err)
+	}
+
+	rd, err := NewReaderDict(dictSize, dictSize)
+	if err != nil {
+		t.Fatalf("NewReaderDict: error %v", err)
+	}
+	rstate := NewReaderState(props, rd)
+	r, err := NewReader(&buf, rstate)
+	if err != nil {
+		t.Fatalf("NewReader error %v", err)
+	}
+	l := LimitedReader{R: r, N: int64(len(p))}
+	q := make([]byte, len(p))
+	n, err = l.Read(q)
+	if err != nil {
+		t.Fatalf("r.Read error %v", err)
+	}
+	if n != len(p) {
+		t.Fatalf("r.Read read %d bytes; want %d", n, len(p))
+	}
+	if !bytes.Equal(p, q) {
+		t.Fatalf("r.Read returned %q; want %q", q, msg)
+	}
+	n, err = l.Read(q)
 	if err != io.EOF {
 		t.Fatalf("r.Read returned %v; want %v", err, io.EOF)
 	}
