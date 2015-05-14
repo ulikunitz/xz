5363defe9071b725412f117695b4b30e2692af6c
Ulrich Kunitz <uli.kunitz@gmail.com>
2015-05-14 21:47:01 +0200

    lzb: improved dictionary implementation
    We should not use top to prevent confusion with the buffer

--- lzb/dict.go
+++ lzb/dict.go
@@ -4,38 +4,40 @@ import "errors"
 
 type dict struct {
 	buf  *buffer
-	top  int64
+	head int64
 	size int64
 }
 
-func makeDict(b *buffer, top int64, size int64) (d *dict, err error) {
+func newDict(b *buffer, head int64, size int64) (d *dict, err error) {
 	switch {
 	case size <= 0:
 		return nil, errors.New("size must be positive")
 	case size > int64(b.capacity()):
 		return nil, errors.New("size exceeds buffer capacity")
-	case !(b.bottom <= top && top <= b.top):
-		return nil, errors.New("top offset out of range")
+	case !(b.bottom <= head && head <= b.top):
+		return nil, errors.New("head offset out of range")
 	}
-	return &dict{buf: b, top: top, size: size}, nil
+	return &dict{buf: b, head: head, size: size}, nil
 }
 
 func (d *dict) byteAt(dist int64) byte {
-	if dist <= 0 {
-		panic("dist must be positve")
+	if !(0 < dist && dist <= d.size) {
+		panic("dist out of range")
 	}
-	off := d.top - dist
+	off := d.head - dist
 	if off < d.buf.bottom {
 		return 0
 	}
 	return d.buf.data[d.buf.index(off)]
 }
 
+var errMove = errors.New("move outside buffer")
+
 func (d *dict) move(n int) error {
-	off := d.top + int64(n)
+	off := d.head + int64(n)
 	if !(d.buf.bottom <= off && off <= d.buf.top) {
-		return errors.New("move outside buffer")
+		return errMove
 	}
-	d.top = off
+	d.head = off
 	return nil
 }
