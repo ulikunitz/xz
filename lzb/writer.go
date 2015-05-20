7f87f71ec29853d120be72a69d2e05a8dcd36bcb
Ulrich Kunitz <uli.kunitz@gmail.com>
2015-05-20 08:26:23 +0200

    lzb: NewWriter methods implemeted and additional method stubs

--- lzb/writer.go
+++ lzb/writer.go
@@ -1,27 +1,70 @@
 package lzb
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+)
 
 // TODO
 //
-// - write seperate OpGenerator type that contains a hashDict
-//     + provide an interface for the OpGenerator (we will have multiple
-//       implementations)
-//     + support transfer of state from hashDict to syncHashDict
-//     + do only simple greedy at first
 // - write fills buffer until full + compression is started at the very
 //   end
-// - ops that hit buf.top will not be used all others will unless all
-//   data must be consumed
 
 type OpFinder interface {
 	findOps(s *State, all bool) ([]operation, error)
 	fmt.Stringer
 }
 
+// Writer produces an LZMA stream. EOS requests Close to write an
+// end-of-stream marker.
 type Writer struct {
 	State    *State
+	EOS      bool
 	OpFinder OpFinder
 	re       *rangeEncoder
+	buf      *buffer
 	closed   bool
+}
+
+func NewWriter(pw io.Writer, p Params) (w *Writer, err error) {
+	buf, err := newBuffer(p.BufferSize + p.DictSize)
+	if err != nil {
+		return nil, err
+	}
+	d, err := newHashDict(buf, p.DictSize)
+	if err != nil {
+		return nil, err
+	}
+	d.sync()
+	state := NewState(p.Properties, d)
+	return NewWriterState(pw, state)
+}
+
+func NewWriterState(pw io.Writer, state *State) (w *Writer, err error) {
+	if _, ok := state.dict.(*hashDict); !ok {
+		return nil, errors.New(
+			"state must support a writer (no hashDict)")
+	}
+	w = &Writer{
+		State:    state,
+		buf:      state.dict.buffer(),
+		re:       newRangeEncoder(pw),
+		OpFinder: Greedy,
+	}
+	return w, nil
+}
+
+// applyOps applies ops on the writer. The method assumes that all
+// operations are correct and no conditions are violated.
+func applyOps(ops []operation) error {
+	panic("TODO")
+}
+
+func Write(p []byte) (n int, err error) {
+	panic("TODO")
+}
+
+func Close() error {
+	panic("TODO")
 }
