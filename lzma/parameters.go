48778554c09fb22049b4a2b0234d97e2afb6e30a
Ulrich Kunitz <uli.kunitz@gmail.com>
2015-05-26 07:52:26 +0200

    lzma: defined Parameters
    lzma requires a clean interface that doesn't use any imported types.
    Therefore this commit defines Parameters in lzma that are identical to
    the Parameters as defined in lzb.

---
+++ lzma/parameters.go
@@ -0,0 +1,50 @@
+package lzma
+
+import "github.com/uli-go/xz/lzb"
+
+// Parameters contain all information required to decode or encode an LZMA
+// stream.
+//
+// The maximum of the dictionary and buffer size is 2^31-1 on 32-bit
+// platforms.
+type Parameters struct {
+	// number of literal context bits
+	LC int
+	// number of literal position bits
+	LP int
+	// number of position bits
+	PB int
+	// size of the dictionary in bytes
+	DictSize int64
+	// size of uncompressed data in bytes
+	Size int64
+	// header includes unpacked size
+	SizeInHeader bool
+	// end-of-stream marker requested
+	EOS bool
+	// buffer size
+	BufferSize int64
+}
+
+// Default defines standard parameters.
+var Default = Parameters{
+	LC:         3,
+	LP:         0,
+	PB:         2,
+	DictSize:   4096,
+	BufferSize: 4096,
+}
+
+// lzbParamertes converts Parameters in the identical lzb.Parameters.
+func lzbParameters(p *Parameters) lzb.Parameters {
+	return lzb.Parameters{
+		LC:           p.LC,
+		LP:           p.LP,
+		PB:           p.PB,
+		DictSize:     p.DictSize,
+		Size:         p.Size,
+		SizeInHeader: p.SizeInHeader,
+		EOS:          p.EOS,
+		BufferSize:   p.BufferSize,
+	}
+}
