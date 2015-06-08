3a6cf1c0bfb45713bb1a37ff437a995d871ec241
Ulrich Kunitz <uli.kunitz@gmail.com>
2015-06-08 08:15:33 +0200

    lzmago: more preset translation

--- cmd/lzmago/lzma.go
+++ cmd/lzmago/lzma.go
@@ -1,5 +1,15 @@
 package main
 
 // translating preset config into lzma params
-// dict_pow2 18, 20, 21, 22, 22, 23, *23*, 24, 25, 26
+// default preset 6
+// dict_pow2 18, 20, 21, 22, 22, 23, 23, 24, 25, 26
 // LC-LP-PB: 3,0,2
+// see tukaani xz implementation lzma_encoder_presets
+// preset <= 3 0: HC3 1: HC4
+// nice_len: 128 : 273
+// depths: 4, 8, 24, 48 
+// MODE_FAST
+// preset > 3: BT4
+// nice_len: 4: 16 5: 32 sonst 64
+// depth 0
+// MODE_NORMAL
