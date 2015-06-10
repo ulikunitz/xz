56f2d175f27324c098ca6955256731db8c04ca38
Ulrich Kunitz <uli.kunitz@gmail.com>
2015-06-10 22:58:31 +0200

    gflag: implemented Parse functions
    The functions are implemented but need to be tested.

--- gflag/flag.go
+++ gflag/flag.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"strconv"
+	"strings"
 )
 
 var CommandLine = NewFlagSet(os.Args[0], ExitOnError)
@@ -27,6 +28,7 @@ const (
 
 type Value interface {
 	Set(string) error
+	Update()
 	Get() interface{}
 	String() string
 }
@@ -63,6 +65,142 @@ func NewFlagSet(name string, errorHandling ErrorHandling) *FlagSet {
 	return f
 }
 
+func Parsed() bool {
+	return CommandLine.parsed
+}
+
+func (f *FlagSet) Parsed() bool {
+	return f.parsed
+}
+
+func Parse() {
+	// errors are ignored because CommandLine is set on ExitOnError
+	CommandLine.Parse(os.Args[1:])
+}
+
+func (f *FlagSet) lookupLongOption(name string) (flag *Flag, err error) {
+	if len(name) < 2 {
+		f.panicf("%s is not a long option", name)
+	}
+	var ok bool
+	if flag, ok = f.formal[name]; !ok {
+		return nil, fmt.Errorf("long option %s is unsupported", name)
+	}
+	if flag.Name != name {
+		f.panicf("got %s flag; want %s flag", flag.Name, name)
+	}
+	return flag, nil
+}
+
+func (f *FlagSet) lookupShortOption(r rune) (flag *Flag, err error) {
+	var ok bool
+	name := string([]rune{r})
+	if flag, ok = f.formal[name]; !ok {
+		return nil, fmt.Errorf("short option %s is unsupported", name)
+	}
+	if !strings.ContainsRune(flag.Shorthands, r) {
+		f.panicf("flag supports shorthands %q; but doesn't contain %s",
+			flag.Shorthands, name)
+	}
+	return flag, nil
+}
+
+func (f *FlagSet) processExtraFlagArg(flag *Flag, i int) error {
+	if flag.HasArg == NoArg {
+		// no argument required
+		flag.Value.Update()
+		return nil
+	}
+	if i < len(f.args) {
+		arg := f.args[i]
+		if len(arg) == 0 || arg[0] != '-' {
+			f.filterArg(i)
+			return flag.Value.Set(arg)
+		}
+	}
+	// no argument
+	if flag.HasArg == RequiredArg {
+		return fmt.Errorf("no argument present")
+	}
+	// flag.HasArg == OptionalArg
+	flag.Value.Update()
+	return nil
+}
+
+func (f *FlagSet) filterArg(i int) {
+	copy(f.args[i:], f.args[i+1:])
+	f.args = f.args[:len(f.args)-1]
+}
+
+func (f *FlagSet) parseArg(i int) (next int, err error) {
+	arg := f.args[i]
+	if len(arg) < 2 || arg[0] != '-' {
+		return i + 1, nil
+	}
+	if arg[1] == '-' {
+		// argument starts with --
+		f.filterArg(i)
+		if len(arg) == 2 {
+			// argument is --; remove it and ignore all
+			// following arguments
+			return len(f.args), nil
+		}
+		arg = arg[2:]
+		flagArg := strings.SplitN(arg, "=", 2)
+		flag, err := f.lookupLongOption(flagArg[0])
+		if err != nil {
+			return i, err
+		}
+		// case 1: no equal sign
+		if len(flagArg) == 1 {
+			err = f.processExtraFlagArg(flag, i)
+			return i, err
+		}
+		// case 2: equal sign
+		if flag.HasArg == NoArg {
+			err = fmt.Errorf("option %s doesn't support argument",
+				arg)
+		} else {
+			err = flag.Value.Set(flagArg[1])
+		}
+		return i, err
+	}
+	// short options
+	f.filterArg(i)
+	arg = arg[1:]
+	for _, r := range arg {
+		flag, err := f.lookupShortOption(r)
+		if err != nil {
+			return i, err
+		}
+		if err = f.processExtraFlagArg(flag, i); err != nil {
+			return i, err
+		}
+	}
+	return i, nil
+}
+
+func (f *FlagSet) Parse(arguments []string) error {
+	f.parsed = true
+	f.args = arguments
+	for i := 0; i < len(f.args); {
+		var err error
+		i, err = f.parseArg(i)
+		if err == nil {
+			continue
+		}
+		switch f.errorHandling {
+		case ContinueOnError:
+			return err
+		case ExitOnError:
+			os.Exit(2)
+		case PanicOnError:
+			panic(err)
+		}
+	}
+	return nil
+}
+
 func (f *FlagSet) out() io.Writer {
 	if f.output == nil {
 		return os.Stderr
@@ -91,12 +229,13 @@ func (b *boolValue) Get() interface{} {
 }
 
 func (b *boolValue) Set(s string) error {
-	if s == "" {
-		*b = boolValue(true)
-	}
 	v, err := strconv.ParseBool(s)
 	*b = boolValue(v)
 	return err
+}
+
+func (b *boolValue) Update() {
+	*b = true
 }
 
 func (b *boolValue) String() string {
