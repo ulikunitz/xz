ed5703ab486cee8307d409cc8d5f11af7d4ad9a1
Ulrich Kunitz <uli.kunitz@gmail.com>
2015-07-05 19:52:16 +0200

    xz: update of version files to v0.3

--- cmd/lzmago/version.go
+++ cmd/lzmago/version.go
@@ -1,7 +1,3 @@
-// Copyright 2015 Ulrich Kunitz. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
-const version = "v0.2-356-g7a4fac6"
+const version = "v0.3"
--- cmd/xb/version.go
+++ cmd/xb/version.go
@@ -1,7 +1,3 @@
-// Copyright 2015 Ulrich Kunitz. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
-const version = "v0.2-318-g86991c8"
+const version = "v0.3"
