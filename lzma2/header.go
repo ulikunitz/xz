33a83b2789e2161c3f2dd36f54d7b5013ecee79a
Ulrich Kunitz <uli.kunitz@gmail.com>
2015-07-07 23:20:02 +0200

    lzma2: implemented headerChunkType

--- lzma2/header.go
+++ lzma2/header.go
@@ -1,6 +1,8 @@
 package lzma2
 
 import (
+	"errors"
+
 	"github.com/uli-go/xz/lzma"
 )
 
@@ -40,6 +42,44 @@ type chunkHeader struct {
 	props    lzma.Properties
 }
 
+const (
+	hEOS  = 0
+	hUD   = 1
+	hU    = 2
+	hL    = 1 << 7
+	hLR   = 1<<7 | 1<<5
+	hLRN  = 1<<7 | 1<<6
+	hLRND = 1<<7 | 1<<6 | 1<<5
+)
+
+var errHeaderByte = errors.New("unsupported chunk header byte")
+
 func headerChunkType(h byte) (c chunkType, err error) {
-	panic("TODO")
+	if h&hL == 0 {
+		// no compression
+		switch h {
+		case hEOS:
+			c = cEOS
+		case hUD:
+			c = cUD
+		case hU:
+			c = cU
+		default:
+			return 0, errHeaderByte
+		}
+		return
+	}
+	switch h & hLRND {
+	case hL:
+		c = cL
+	case hLR:
+		c = cLR
+	case hLRN:
+		c = cLRN
+	case hLRND:
+		c = cLRND
+	default:
+		return 0, errHeaderByte
+	}
+	return
 }
--- lzma2/header_test.go
+++ lzma2/header_test.go
@@ -25,3 +25,32 @@ func TestChunkTypeString(t *testing.T) {
 		}
 	}
 }
+
+func TestHeaderChunkType(t *testing.T) {
+	tests := []struct {
+		h byte
+		c chunkType
+	}{
+		{h: 0, c: cEOS},
+		{h: 1, c: cUD},
+		{h: 2, c: cU},
+		{h: 1<<7 | 0x1f, c: cL},
+		{h: 1<<7 | 1<<5 | 0x1f, c: cLR},
+		{h: 1<<7 | 1<<6 | 0x1f, c: cLRN},
+		{h: 1<<7 | 1<<6 | 1<<5 | 0x1f, c: cLRND},
+		{h: 1<<7 | 1<<6 | 1<<5, c: cLRND},
+	}
+	if _, err := headerChunkType(3); err == nil {
+		t.Fatalf("headerChunkType(%d) got %v; want %v",
+			3, err, errHeaderByte)
+	}
+	for _, tc := range tests {
+		c, err := headerChunkType(tc.h)
+		if err != nil {
+			t.Fatalf("headerChunkType error %s", err)
+		}
+		if c != tc.c {
+			t.Errorf("got %s; want %s", c, tc.c)
+		}
+	}
+}
