6850c91422216d8383627daf8a62e4f51e0ec42b
Ulrich Kunitz <uli.kunitz@gmail.com>
2015-07-08 21:16:54 +0200

    term: added the final dot to the IsTerminal documentation comment

--- term/isterminal.go
+++ term/isterminal.go
@@ -12,7 +12,7 @@ import (
 	"unsafe"
 )
 
-// IsTerminal returns true if the given file descriptor is a terminal
+// IsTerminal returns true if the given file descriptor is a terminal.
 func IsTerminal(fd uintptr) bool {
 	var termios syscall.Termios
 	_, _, err := syscall.Syscall6(syscall.SYS_IOCTL, fd,
