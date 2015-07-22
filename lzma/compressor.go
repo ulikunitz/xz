eaa4ebd3e4e0b2d8b02e637fab985338233164b9
Ulrich Kunitz <uli.kunitz@gmail.com>
2015-07-22 23:04:24 +0200

    lzma: added opLenMargin constant with comment

--- lzma/compressor.go
+++ lzma/compressor.go
@@ -2,6 +2,11 @@ package lzma
 
 import "io"
 
+// opLenMargin provides an upper limit for the encoding of a single
+// operation. The value assumes that all range-encoded bits require
+// actually two bits. A typical operations will be shorter.
+const opLenMargin = 10
+
 type Compressor struct {
 }
 
