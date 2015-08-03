7cd5ff8d1af9511a88af6f7267994f43bfe6a8ba
Ulrich Kunitz <uli.kunitz@gmail.com>
2015-08-03 23:03:10 +0200

    lzma: implementation of NewDecompressor

--- lzma/decompressor.go
+++ lzma/decompressor.go
@@ -1,6 +1,9 @@
 package lzma
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+)
 
 // DecompressorParams provides the parameters for the decompressor
 // instances.
@@ -57,4 +60,62 @@ func (p *DecompressorParams) Verify() error {
 
 // Decompressor represents the Decompressor status.
 type Decompressor struct {
+	state      *State
+	rd         *rangeDecoder
+	properties Properties
+	dict       *syncDict
+	// read head
+	head int64
+	// start is the offset from the start of the uncompressed stream
+	start int64
+	// length gives the length of the uncompressed stream; if 0 no
+	// length has been given
+	length int64
+	// filled marks decompressors that will not get more data put
+	// into the dictionary
+	filled bool
+	// eos is set if a eos marker is expected
+	eos bool
+}
+
+func NewDecompressor(lzma io.Reader, p DecompressorParams) (d *Decompressor, err error) {
+	if err = p.Verify(); err != nil {
+		return nil, err
+	}
+	buf, err := newBuffer(p.DictSize)
+	if err != nil {
+		return
+	}
+	dict, err := newSyncDict(buf, p.DictSize)
+	if err != nil {
+		return
+	}
+	props := p.Properties()
+	state := NewState(props, dict)
+	// TODO: convert lzma into a limited reader if CompressedLen > 0
+	lr := lzma
+	if p.CompressedLen > 0 {
+		lr = io.LimitReader(lzma, p.CompressedLen)
+	}
+	rd, err := newRangeDecoder(lr)
+	if err != nil {
+		return nil, err
+	}
+	d = &Decompressor{
+		state:      state,
+		rd:         rd,
+		properties: props,
+		dict:       dict,
+		length:     p.UncompressedLen,
+		eos:        p.EOS,
+	}
+	return d, nil
+}
+
+func (d *Decompressor) Read(p []byte) (n int, err error) {
+	panic("TODO")
+}
+
+func (d *Decompressor) Decompress() error {
+	panic("TODO")
 }
