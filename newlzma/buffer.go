fe53195e241b2c576fea8783a9ac84e30b94010b
Ulrich Kunitz <uli-go@users.noreply.github.com>
2015-08-11 21:16:57 +0200

    newlzma: implemented new circular buffer
    The test file covers all code reachable on the AMD64 platform.

---
+++ newlzma/buffer.go
@@ -0,0 +1,138 @@
+package newlzma
+
+import "errors"
+
+// computeMaxBufferCapacity returns the maximum buffer capacity support by the
+// platform. On 64-bit platforms the maximum buffer capacity is 2^32-1
+// and on 32-bit platforms 2^31-1. The 64-bit platform limit is given by
+// the LZMA specification and the 32-bit limit by the limitations of the
+// platform.
+func computeMaxBufferCapacity() int64 {
+	const c = 1<<32 - 1
+	if int(c) < 0 {
+		return 1<<31 - 2
+	}
+	return c
+}
+
+// maxBufferCapacity provides the maximum buffer capacity supported on
+// the platform. Note that no check is done for the availability of the
+// memory. On 32-bit platforms it should be 2^31-2 and on 64-bit platforms
+// 2^32-1.
+var maxBufferCapacity = computeMaxBufferCapacity()
+
+// buffer provides a circular buffer with at most 2^32-1 bytes capacity.
+// We use the single free byte mechanism to distinguish between empty
+// and full buffer.
+type buffer struct {
+	data  []byte
+	front uint32
+	rear  uint32
+}
+
+// newBuffer creates a new buffer. The function might fail if n exceeds
+// the maximum buffer capacity or n is negative. We
+func newBuffer(capacity int) (b *buffer, err error) {
+	if !(0 < capacity && int64(capacity) <= maxBufferCapacity) {
+		return nil, errors.New("buffer capacity out of range")
+	}
+	return &buffer{data: make([]byte, capacity+1)}, nil
+}
+
+// Cap returns the capacity of the buffer.
+func (b *buffer) Cap() int {
+	return len(b.data) - 1
+}
+
+// Len provides the number of bytes buffered.
+func (b *buffer) Buffered() int {
+	delta := int(b.front) - int(b.rear)
+	if delta < 0 {
+		return len(b.data) + delta
+	}
+	return delta
+}
+
+// Available provides the number of bytes available for writing.
+func (b *buffer) Available() int {
+	delta := int(b.rear) - int(b.front) - 1
+	if delta < 0 {
+		return len(b.data) + delta
+	}
+	return delta
+}
+
+// Read reads byte from the buffer into p and returns the number of
+// bytes read. It never returns an error.
+func (b *buffer) Read(p []byte) (n int, err error) {
+	if b.rear > b.front {
+		k := copy(p, b.data[b.rear:])
+		b.rear += uint32(k)
+		if int(b.rear) == len(b.data) {
+			b.rear = 0
+		}
+		if k == len(p) {
+			return k, nil
+		}
+		p = p[k:]
+		n = k
+	}
+	k := copy(p, b.data[b.rear:b.front])
+	b.rear += uint32(k)
+	return n + k, nil
+}
+
+// Discard skips the n next bytes to read from the buffer, returning the
+// bytes discarded.
+//
+// If Discards skips fewer than n bytes, it returns an error.
+func (b *buffer) Discard(n int) (discarded int, err error) {
+	if n < 0 {
+		panic("negative argument")
+	}
+	if b.rear > b.front {
+		k := len(b.data) - int(b.rear)
+		if n < k {
+			b.rear += uint32(n)
+			return n, nil
+		}
+		discarded += k
+		b.rear = 0
+		n -= k
+	}
+	k := int(b.front) - int(b.rear)
+	if n <= k {
+		b.rear += uint32(n)
+		return discarded + n, nil
+	}
+	b.rear += uint32(k)
+	return discarded + k, errors.New("discarded less bytes then requested")
+}
+
+// Write puts data into the  buffer. If less bytes are written than
+// requested an error is returned.
+func (b *buffer) Write(p []byte) (n int, err error) {
+	r := int(b.rear) - 1
+	if r < 0 {
+		r += len(b.data)
+	}
+	if int(b.front) > r {
+		k := copy(b.data[b.front:], p)
+		b.front += uint32(k)
+		if int(b.front) == len(b.data) {
+			b.front = 0
+		}
+		if k == len(p) {
+			return k, nil
+		}
+		p = p[k:]
+		n += k
+	}
+	k := copy(b.data[b.front:r], p)
+	b.front += uint32(k)
+	n += k
+	if k < len(p) {
+		return n, errors.New("not all data written")
+	}
+	return n, nil
+}
---
+++ newlzma/buffer_test.go
@@ -0,0 +1,221 @@
+package newlzma
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestMaxBufferCapacity(t *testing.T) {
+	const (
+		cap32 = 1<<31 - 2
+		cap64 = 1<<32 - 1
+	)
+	c := int64(cap64)
+	if int(c) < 0 {
+		c = cap32
+	}
+	if maxBufferCapacity != c {
+		t.Fatalf("maxBufferCapacity %#x; want %#x",
+			maxBufferCapacity, c)
+	}
+}
+
+func TestBuffer_Write(t *testing.T) {
+	if _, err := newBuffer(-1); err == nil {
+		t.Fatalf("newBuffer(-1) want error")
+	}
+	buf, err := newBuffer(10)
+	if err != nil {
+		t.Fatalf("newBuffer error %s", err)
+	}
+	b := []byte("1234567890")
+	for i := range b {
+		n, err := buf.Write(b[i : i+1])
+		if err != nil {
+			t.Fatalf("buf.Write(b[%d:%d]) error %s", i, i+1, err)
+		}
+		if n != 1 {
+			t.Fatalf("buf.Write(b[%d:%d]) returned %d; want %d",
+				i, i+1, n, 1)
+		}
+	}
+	const c = 8
+	n, err := buf.Discard(c)
+	if err != nil {
+		t.Fatalf("Discard error %s", err)
+	}
+	if n != c {
+		t.Fatalf("Discard returned %d; want %d", n, c)
+	}
+	buffered := buf.Buffered()
+	available := buf.Available()
+	capacity := buf.Cap()
+	if buffered+available != capacity {
+		t.Logf("buffered %d available %d capacity %d",
+			buffered, available, capacity)
+		t.Fatal("buffered + available != capacity")
+	}
+	n, err = buf.Write(b)
+	if err == nil {
+		t.Fatalf("Write length exceed returned no error; n %d", n)
+	}
+	if n != c {
+		t.Fatalf("Write length exceeding returned %d; want %d", n, c)
+	}
+	n, err = buf.Discard(4)
+	if err != nil {
+		t.Fatalf("Discard error %s", err)
+	}
+	if n != 4 {
+		t.Fatalf("Discard returned %d; want %d", n, 4)
+	}
+	n, err = buf.Write(b[:3])
+	if err != nil {
+		t.Fatalf("buf.Write(b[:3]) error %s; n %d", err, n)
+	}
+	if n != 3 {
+		t.Fatalf("buf.Write(b[:3]) returned %d; want %d", n, 3)
+	}
+}
+
+func TestBuffer_Buffered_Available(t *testing.T) {
+	buf, err := newBuffer(10)
+	if err != nil {
+		t.Fatalf("newBuffer(10) error %s", err)
+	}
+	b := []byte("0123456789")
+	if _, err = buf.Write(b); err != nil {
+		t.Fatalf("buf.Write(b) error %s", err)
+	}
+	if n := buf.Buffered(); n != 10 {
+		t.Fatalf("buf.Buffered() returns %d; want %d", n, 10)
+	}
+	if n := buf.Available(); n != 0 {
+		t.Fatalf("buf.Available() returns %d; want %d", n, 0)
+	}
+	if _, err = buf.Discard(8); err != nil {
+		t.Fatalf("buf.Discard(8) error %s", err)
+	}
+	if _, err = buf.Write(b[:7]); err != nil {
+		t.Fatalf("buf.Write(b[:7]) error %s", err)
+	}
+	if n := buf.Buffered(); n != 9 {
+		t.Fatalf("buf.Buffered() returns %d; want %d", n, 9)
+	}
+	if n := buf.Available(); n != 1 {
+		t.Fatalf("buf.Available() returns %d; want %d", n, 1)
+	}
+}
+
+func TestBuffer_Read(t *testing.T) {
+	buf, err := newBuffer(10)
+	if err != nil {
+		t.Fatalf("newBuffer(10) error %s", err)
+	}
+	b := []byte("0123456789")
+	if _, err = buf.Write(b); err != nil {
+		t.Fatalf("buf.Write(b) error %s", err)
+	}
+	p := make([]byte, 8)
+	n, err := buf.Read(p)
+	if err != nil {
+		t.Fatalf("buf.Read(p) error %s", err)
+	}
+	if n != len(p) {
+		t.Fatalf("buf.Read(p) returned %d; want %d", n, len(p))
+	}
+	if !bytes.Equal(p, b[:8]) {
+		t.Fatalf("buf.Read(p) put %s into p; want %s", p, b[:8])
+	}
+	if _, err = buf.Write(b[:7]); err != nil {
+		t.Fatalf("buf.Write(b[:7]) error %s", err)
+	}
+	q := make([]byte, 7)
+	n, err = buf.Read(q)
+	if err != nil {
+		t.Fatalf("buf.Read(q) error %s", err)
+	}
+	if n != len(q) {
+		t.Fatalf("buf.Read(q) returns %d; want %d", n, len(q))
+	}
+	c := []byte("8901234")
+	if !bytes.Equal(q, c) {
+		t.Fatalf("buf.Read(q) put %s into q; want %s", q, c)
+	}
+	if _, err := buf.Write(b[7:]); err != nil {
+		t.Fatalf("buf.Write(b[7:]) error %s", err)
+	}
+	if _, err := buf.Write(b[:2]); err != nil {
+		t.Fatalf("buf.Write(b[:2]) error %s", err)
+	}
+	t.Logf("buf.rear %d buf.front %d", buf.rear, buf.front)
+	r := make([]byte, 2)
+	n, err = buf.Read(r)
+	if err != nil {
+		t.Fatalf("buf.Read(r) error %s", err)
+	}
+	if n != len(r) {
+		t.Fatalf("buf.Read(r) returns %d; want %d", n, len(r))
+	}
+	d := []byte("56")
+	if !bytes.Equal(r, d) {
+		t.Fatalf("buf.Read(r) put %s into r; want %s", r, d)
+	}
+}
+
+func TestBuffer_Discard(t *testing.T) {
+	buf, err := newBuffer(10)
+	if err != nil {
+		t.Fatalf("newBuffer(10) error %s", err)
+	}
+	b := []byte("0123456789")
+	if _, err = buf.Write(b); err != nil {
+		t.Fatalf("buf.Write(b) error %s", err)
+	}
+	n, err := buf.Discard(11)
+	if err == nil {
+		t.Fatalf("buf.Discard(11) didn't return error")
+	}
+	if n != 10 {
+		t.Fatalf("buf.Discard(11) returned %d; want %d", n, 10)
+	}
+	if _, err := buf.Write(b); err != nil {
+		t.Fatalf("buf.Write(b) #2 error %s", err)
+	}
+	n, err = buf.Discard(10)
+	if err != nil {
+		t.Fatalf("buf.Discard(10) error %s", err)
+	}
+	if n != 10 {
+		t.Fatalf("buf.Discard(11) returned %d; want %d", n, 10)
+	}
+	if _, err := buf.Write(b[:4]); err != nil {
+		t.Fatalf("buf.Write(b[:4]) error %s", err)
+	}
+	n, err = buf.Discard(1)
+	if err != nil {
+		t.Fatalf("buf.Discard(1) error %s", err)
+	}
+	if n != 1 {
+		t.Fatalf("buf.Discard(1) returned %d; want %d", n, 1)
+	}
+}
+
+func TestBuffer_Discard_panic(t *testing.T) {
+	buf, err := newBuffer(10)
+	if err != nil {
+		t.Fatalf("newBuffer(10) error %s", err)
+	}
+	panicked := false
+	func() {
+		defer func() {
+			if x := recover(); x != nil {
+				panicked = true
+			}
+		}()
+		buf.Discard(-1)
+	}()
+	if !panicked {
+		t.Fatalf("buf.Discard(-1) didn't panic")
+	}
+}
