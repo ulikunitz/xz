b2960ca8a9a572262dd203fef3e510664fe47e3c
Ulrich Kunitz <uli-go@users.noreply.github.com>
2015-08-11 09:42:12 +0200

    newlzma: created package newlzma
    This package is a new start for creating a decompressor and compressor
    for LZMA streams. It will redesign the buffers and dictionaries.

---
+++ newlzma/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2015 Ulrich Kunitz. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package newlzma allows the decoding and encoding of LZMA streams.
+//
+// The package is written completely in Go and doesn't rely on any external
+// library.
+package newlzma
