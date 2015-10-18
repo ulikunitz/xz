c811c256e6b0da287e09e6cca303fede3cf4f34b
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2015-10-18 21:00:08 +0200

    lzma: documented method stubs for new encoder dictionary

--- lzma/encoderdict.go
+++ lzma/encoderdict.go
@@ -150,3 +150,106 @@ func (e *encoderDict) CopyN(w io.Writer, n int) (written int, err error) {
 	k, err := io.CopyN(w, &buf, int64(n))
 	return int(k), err
 }
+
+// TODO: New EncoderDict type for the new simplified encoder.
+
+// EncoderDict provides the dictionary for the encoder. It includes a
+// matcher for searching matching strings in the dictionary. Note that
+// the dictionary also supports a buffer of data that has yet to be
+// moved into the dictionary.
+type EncoderDict struct {
+	buf      buffer
+	m        matcher
+	head     int
+	capacity int
+}
+
+// Creates a new encoder dictionary. The initial position and length of
+// the dictionary will be zero. There will be no buffered data.
+func NewEncoderDict(dictCap, bufCap int) (ed *EncoderDict, err error) {
+	panic("TODO")
+}
+
+// Resets the dictionary and sets the position to the given value. The
+// dictionary the will be cleared and the buffered data maybe discarded
+// based on the value of pos.
+func (ed *EncoderDict) Reset(pos int64) error {
+	panic("TODO")
+}
+
+// Available returns the number of bytes that can be buffered by a
+// following Write call.
+func (ed *EncoderDict) Available() int {
+	panic("TODO")
+}
+
+// Buffered gives the number of bytes available for a following Read or
+// Advance.
+func (ed *EncoderDict) Buffered() int {
+	panic("TODO")
+}
+
+// Len returns the number of bytes stored in the dictionary after the
+// current position. It may be larger then the dictionary length.
+func (ed *EncoderDict) Len() int {
+	panic("TODO")
+}
+
+// DictCap returns the dictionary capacity.
+func (ed *EncoderDict) DictCap() int {
+	return ed.capacity
+}
+
+// BufCap returns the buffer capacity.
+func (ed *EncoderDict) BufCap() int {
+	panic("TODO")
+}
+
+// DictLen returns the current number of bytes of the dictionary. The
+// number has dictCap as upper limit.
+func (ed *EncoderDict) DictLen() int {
+	n := ed.Len()
+	if n > ed.capacity {
+		return ed.capacity
+	}
+	return n
+}
+
+// Returns the current position of the dictionary head.
+func (ed *EncoderDict) Pos() int64 {
+	panic("TODO")
+}
+
+// ByteAt returns a byte from the dictionary. The distance is the
+// positive difference from the current head. A distance of 1 will
+// return the top-most byte in the dictionary.
+func (ed *EncoderDict) ByteAt(distance int) byte {
+	panic("TODO")
+}
+
+// Write puts new data into the dictionary.
+func (ed *EncoderDict) Write(p []byte) (n int, err error) {
+	panic("TODO")
+}
+
+// Read reads data from the buffer in front of the dictionary. Reading
+// has the same effect as Advance on the dictionary.
+func (ed *EncoderDict) Read(p []byte) (n int, err error) {
+	panic("TODO")
+}
+
+// Advance moves the dictionary head ahead by the given number of bytes.
+func (ed *EncoderDict) Advance(n int) (advanced int, err error) {
+	panic("TODO")
+}
+
+// CopyN copies the n topmost bytes of the dictionary. The maximum for n
+// is given by the Len() method.
+func (ed *EncoderDict) CopyN(w io.Writer, n int64) (written int64, err error) {
+	panic("TODO")
+}
+
+// Discard discards data at the end of the dictionary buffer.
+func (ed *EncoderDict) Discard(n int) (discarded int, err error) {
+	panic("TODO")
+}
