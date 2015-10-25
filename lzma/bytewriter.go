ebdee8612fae84f6d9bf733de6bf733b75dbe177
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2015-10-25 14:12:58 +0100

    lzma: remove ByteWriterFromWriter
    This functionality is provided by bufio.Writer, so we don't need a cheap
    version of this here.

--- lzma/bytewriter.go
+++ lzma/bytewriter.go
@@ -5,30 +5,6 @@ import (
 	"io"
 )
 
-// bwriter implements a ByteWriter on top of a Writer
-type bwriter struct {
-	w   io.Writer
-	buf []byte
-}
-
-// WriteByte writes a byte using the standard writer. Note that the
-// function is not thread-safe.
-func (bw *bwriter) WriteByte(c byte) error {
-	bw.buf[0] = c
-	_, err := bw.w.Write(bw.buf)
-	return err
-}
-
-// ByteWriterFromWriter converts a Writer to a ByteWriter. If the Writer
-// doesn't support the ByteWriter interface directly a new structure
-// will be allocated and a non-thread-safe ByteWriter will be returned.
-func ByteWriterFromWriter(w io.Writer) io.ByteWriter {
-	if bw, ok := w.(io.ByteWriter); ok {
-		return bw
-	}
-	return &bwriter{w, make([]byte, 1)}
-}
-
 // ErrLimit indicates that the limit has been reached.
 var ErrLimit = errors.New("limit reached")
 
