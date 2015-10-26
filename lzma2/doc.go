4f6198adda34f97239eb8e75376569d20d04a14c
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2015-10-26 23:36:11 +0100

    doc.go: provide package documentation in a separate file
    As I learned providing the package documentation in a normal file
    creates an issue if the file is removed from the source code tree,
    because no description will be available. A separate doc.go file
    addresses the problem.

---
+++ lzma2/doc.go
@@ -0,0 +1,4 @@
+// Package lzma2 provides readers and writers for the LZMA2 format. The
+// format adds the capabilities flushing, parallel compression and
+// uncompressed segments to the LZMA algorithm.
+package lzma2
