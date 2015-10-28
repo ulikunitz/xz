1bc17de809517358ae796df53a184c9186cf8cc6
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2015-10-28 08:18:06 +0100

    lzma2: created stubs for the main types of the package
    The commit provides stubs for the types Reader and Writer for reading
    and writing LZMA2 chunk sequences as well as a FileReader and FileWriter
    for reading and writing LZMA2 files with a leading dictionary capacity
    byte and a terminating end-of-stream chunk.

---
+++ lzma2/reader.go
@@ -0,0 +1,49 @@
+package lzma2
+
+import (
+	"errors"
+	"io"
+)
+
+// The EOS error indicates that an LZMA2 end-of-stream chunk has been
+// encountered.
+var EOS = errors.New("lzma2 end-of-stream")
+
+// A reader supports the reading of LZMA2 chunk sequences. Note that the
+// first chunk must have at least a property reset and should have a
+// dictionary reset. The chunk sequence may not be terminated by an
+// end-of-stream chunk.
+type Reader struct {
+	// TODO
+}
+
+// NewReader creates a reader for an LZMA2 chunk sequence with the given
+// dictionary capacity.
+func NewReader(lzma2 io.Reader, dictCap int) (r *Reader, err error) {
+	panic("TODO")
+}
+
+// Read reads data from the LZMA2 chunk sequence. If an end-of-stream
+// chunk is encountered EOS is returned, it the sequence stops without
+// an end-of-stream chunk io.EOF is returned.
+func (r *Reader) Read(p []byte) (n int, err error) {
+	panic("TODO")
+}
+
+// The file reader supports the reading of LZMA2 files, where the first
+// chunk is preceded by the dictionary size.
+type FileReader struct {
+	r Reader
+}
+
+// NewFileReader creates a reader for LZMA2 files, where the dictionary
+// capacity is encoded in the first byte.
+func NewFileReader(lzma2File io.Reader) (r *FileReader, err error) {
+	panic("TODO")
+}
+
+// Reads data from the file reader. It returns io.EOF if the end of the
+// file is encountered.
+func (r *FileReader) Read(p []byte) (n int, err error) {
+	panic("TODO")
+}
---
+++ lzma2/writer.go
@@ -0,0 +1,69 @@
+package lzma2
+
+import "io"
+
+// Parameters describe the parameters for an LZMA2 writer.
+type Parameters struct {
+	LC      int
+	LP      int
+	PB      int
+	DictCap int
+	BufSize int
+}
+
+// Writer writes a sequence of LZMA2 chunks. The first chunk will always
+// reset the dictionary. The Writer will not terminate the chunk
+// sequence with an end-of-stream chunk. Use WriteEOS for writing the
+// end of stream chunk.
+type Writer struct {
+	// TODO
+}
+
+// NewWriter creates an LZMA2 chunk sequence writer with the default
+// parameters.
+func NewWriter(lzma2 io.Writer) (w *Writer, err error) {
+	panic("TODO")
+}
+
+// NewWriterParams creates an LZMA2 chunk stream writer with the given
+// parameters.
+func NewWriterParams(lzma2 io.Writer, params Parameters) (w *Writer,
+	err error) {
+
+	panic("TODO")
+}
+
+// Writes data to the LZMA2 chunk stream.
+func (w *Writer) Write(p []byte) (n int, err error) {
+	panic("TODO")
+}
+
+// Flush terminates the current chunk. If additional data is provided a
+// new chunk will be created.
+func (w *Writer) Flush() error {
+	panic("TODO")
+}
+
+// Close terminates the chunk sequence. It doesn't write an
+// end-of-stream chunk. Use WriteEOS to write such a chunk.
+func (w *Writer) Close() error {
+	panic("TODO")
+}
+
+// The FileWriter writes LZMA2 files with a leading dictionary capacity
+// byte and an end-of-stream chunk.
+type FileWriter struct {
+	Writer
+}
+
+// NewFileWriter creates an LZMA2 file writer. It writes the initial
+// dictionary capacity byte.
+func NewFileWriter(lzma2 io.Writer) (w *FileWriter, err error) {
+	panic("TODO")
+}
+
+// Close closes the file writer by terminating the active chunk and
+// writing an end-of-stream chunk.
+func (w *FileWriter) Close() error {
+	panic("TODO")
+}
