f1b1cc2b8a18c6324a705922e67826a16a0324fb
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2015-10-28 08:23:23 +0100

    lzma2: extended the package description

--- lzma2/doc.go
+++ lzma2/doc.go
@@ -1,4 +1,11 @@
 // Package lzma2 provides readers and writers for the LZMA2 format. The
 // format adds the capabilities flushing, parallel compression and
 // uncompressed segments to the LZMA algorithm.
+//
+// The Reader and Writer allows the reading and writing of LZMA2 chunk
+// sequences. They can be used to parallel compress or decompress files.
+//
+// FileReader and FileWriter allow the encoding and decoding of LZMA2
+// files sequentially. The first LZMA2 chunk is preceded by dictionary
+// capacity byte and the files include the end-of-stream chunk.
 package lzma2
