5c8304b5cd601887e13c936ac21b75cac372e2af
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2015-11-17 08:18:51 +0100

    lzma2: simple LZMA2 writer test added
    The test runs succesfully.

--- lzma2/writer_test.go
+++ lzma2/writer_test.go
@@ -7,8 +7,22 @@ import (
 
 func TestWriter(t *testing.T) {
 	var buf bytes.Buffer
-	_, err := NewWriter(&buf)
+	w, err := NewWriter(&buf)
 	if err != nil {
-		t.Fatalf("NewWriter error %s")
+		t.Fatalf("NewWriter error %s", err)
+	}
+	if _, err = w.Write([]byte{'a'}); err != nil {
+		t.Fatalf("w.Write([]byte{'a'}) error %s", err)
+	}
+	if err = w.Flush(); err != nil {
+		t.Fatalf("w.Flush() error %s", err)
+	}
+	if err = w.Close(); err != nil {
+		t.Fatalf("w.Close() error %s", err)
+	}
+	p := buf.Bytes()
+	want := []byte{1, 0, 0, 'a'}
+	if !bytes.Equal(p, want) {
+		t.Fatalf("bytes written %#v; want %#v", p, want)
 	}
 }
