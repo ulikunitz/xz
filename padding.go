bda234fd36095fa4fcf839cf3a1f5bb8bdb2ce2e
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2015-12-04 23:47:30 +0100

    xz: removed error field of padReader and padWriter
    I want to keep the code as simple as possible. At this time it is not
    clear whether error capture in the reader and writers is required.

--- padding.go
+++ padding.go
@@ -12,7 +12,6 @@ type padWriter struct {
 	n         int64
 	blockSize int
 	pad       []byte
-	err       error
 }
 
 // newPadWriter creates a new pad writer.
@@ -29,26 +28,18 @@ func newPadWriter(w io.Writer, blockSize int) *padWriter {
 
 // Write writes the slide to the underlying writer.
 func (w *padWriter) Write(p []byte) (n int, err error) {
-	if w.err != nil {
-		return 0, w.err
-	}
 	n, err = w.w.Write(p)
 	w.n += int64(n)
-	w.err = err
 	return n, err
 }
 
 // Pad writes the pad to align the stream with the block size.
 func (w *padWriter) Pad() (n int, err error) {
-	if w.err != nil {
-		return 0, w.err
-	}
 	k := int(w.n % int64(w.blockSize))
 	if k == 0 {
 		return 0, nil
 	}
 	n, err = w.Write(w.pad[:w.blockSize-k])
-	w.err = err
 	return n, err
 }
 
@@ -58,7 +49,6 @@ type padReader struct {
 	n         int64
 	blockSize int
 	p         []byte
-	err       error
 }
 
 // newPadReader creates a new pad reader.
@@ -75,32 +65,23 @@ func newPadReader(r io.Reader, blockSize int) *padReader {
 
 // Read reads data from the underlying reader.
 func (r *padReader) Read(p []byte) (n int, err error) {
-	if r.err != nil {
-		return 0, r.err
-	}
 	n, err = r.r.Read(p)
 	r.n += int64(n)
-	r.err = err
 	return n, err
 }
 
 // Pad reads the padding from the underlying reader.
 func (r *padReader) Pad() (n int, err error) {
-	if r.err != nil {
-		return 0, r.err
-	}
 	k := int(r.n % int64(r.blockSize))
 	if k == 0 {
 		return 0, nil
 	}
 	if n, err = io.ReadFull(r, r.p[:r.blockSize-k]); err != nil {
-		r.err = err
 		return n, err
 	}
 	for _, c := range r.p[:n] {
 		if c != 0 {
-			r.err = errors.New("xz: non-zero padding byte")
-			return n, r.err
+			return n, errors.New("xz: non-zero padding byte")
 		}
 	}
 	return n, nil
