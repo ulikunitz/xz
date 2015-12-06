bf995c5b37ad0f58b9469cfc615e0ee6f4759664
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2015-12-06 10:16:11 +0100

    lzma2: WriteEOS removed
    The Close methods of the lzma2.Writer writes an EOS chunk if called.
    Therefore a special method to write an EOS chunk is not required
    anymore. So it can be removed.

--- lzma2/header.go
+++ lzma2/header.go
@@ -255,14 +255,6 @@ func putUint16BE(p []byte, x uint16) {
 	p[1] = byte(x)
 }
 
-// WriteEOS writes a null byte indicating the end of the stream. The end
-// of stream marker must always be present in an LZMA2 stream.
-func WriteEOS(w io.Writer) error {
-	var p [1]byte
-	_, err := w.Write(p[:])
-	return err
-}
-
 // chunkState is used to manage the state of the chunks
 type chunkState byte
 
--- lzma2/header_test.go
+++ lzma2/header_test.go
@@ -128,21 +128,6 @@ func TestReadChunkHeader(t *testing.T) {
 	}
 }
 
-func TestWriteEOS(t *testing.T) {
-	buf := new(bytes.Buffer)
-	err := WriteEOS(buf)
-	if err != nil {
-		t.Fatalf("WriteEOS: unexpected error %s", err)
-	}
-	b := buf.Bytes()
-	if len(b) != 1 {
-		t.Fatalf("len(b) is %d; want %d", len(b), 1)
-	}
-	if b[0] != 0 {
-		t.Fatalf("b[0] is %#4x; want %#4x", b[0], byte(0))
-	}
-}
-
 func TestReadEOS(t *testing.T) {
 	var b [1]byte
 	r := bytes.NewReader(b[:])
