5409656ff1e59da9c52dbc6d3cf1b745352a962b
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2015-12-25 12:21:04 +0100

    lzma: updated package documentation

--- lzma/doc.go
+++ lzma/doc.go
@@ -2,14 +2,11 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package lzma allows the decoding and encoding of LZMA streams.
-//
-// It provides a Writer and a Reader for decoding and encoding of LZMA
-// stream with headers.
-//
-// The types Encoder and Decoder are provided to allow the decoding and
-// encoding of raw LZMA streams without headers. The lzma2 package uses
-// these types.
+// Package lzma allows the decoding and encoding of LZMA streams. The
+// Reader and Writer supports the decoding and encoding of LZMA streams
+// with headers. The types Encoder and Decoder are provided to allow the
+// decoding and encoding of raw LZMA streams without headers. The lzma2
+// package uses these types.
 //
 // The package is written completely in Go and doesn't rely on any external
 // library.
