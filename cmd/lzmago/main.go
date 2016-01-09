3ef21e2e1933cf1a1722539bba48bc0a1f61761e
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2016-01-09 19:13:58 +0100

    cmd: added documentation lines for lzmago and xb commands.

--- cmd/lzmago/main.go
+++ cmd/lzmago/main.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Command lzmago supports the compression and decompression of LZMA files.
 package main
 
 //go:generate xb cat -o licenses.go xzLicense:github.com/ulikunitz/xz/LICENSE goLicense:~/go/LICENSE
--- cmd/xb/main.go
+++ cmd/xb/main.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Command xb supports the xz for Go project builds.
 package main
 
 //go:generate xb version-file -o version.go
