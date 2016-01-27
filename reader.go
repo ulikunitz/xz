ae910992437e782e4f01f91f4334959ee5b67b6b
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2016-01-27 08:13:54 +0100

    Added and fixed copyright statements.

--- reader.go
+++ reader.go
@@ -1,3 +1,7 @@
+// Copyright 2014-2016 Ulrich Kunitz. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package xz supports the compression and decompression of xz files.
 package xz
 
