6167ff04e38481cf95ef84acaaca5d66cb780ef3
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2016-02-17 22:58:41 +0100

    lzma: added documentation to the binary tree methods
    The method find has been renamed in the more properly named search.

--- lzma/bintree.go
+++ lzma/bintree.go
@@ -8,6 +8,7 @@ import (
 	"unicode"
 )
 
+// node represents a node in the binary tree.
 type node struct {
 	// x is the search value
 	x uint32
@@ -22,16 +23,32 @@ type node struct {
 // wordLen is the number of bytes represented by the v field of a node.
 const wordLen = 4
 
+// binTree supports the identification of the next operation based on a
+// binary tree.
+//
+// Nodes will be identified by their index into the ring buffer.
 type binTree struct {
-	node  []node
-	hoff  int64
+	// ring buffer of nodes
+	node []node
+	// absolute offset of the entry for the next node. Position 4
+	// byte larger.
+	hoff int64
+	// front position in the node ring buffer
 	front uint32
-	root  uint32
-	x     uint32
+	// index of the root node
+	root uint32
+	// current x value
+	x uint32
 }
 
+// null represents the nonexistent index. We can't use zero because it
+// would always exist or we would need to decrease the index for each
+// reference.
 const null uint32 = 1<<32 - 1
 
+// newBinTree initializes the binTree structure. The capacity defines
+// the size of the buffer and defines the maximum distance for which
+// matches will be found.
 func newBinTree(capacity int) (t *binTree, err error) {
 	if capacity < 1 {
 		return nil, errors.New(
@@ -49,6 +66,7 @@ func newBinTree(capacity int) (t *binTree, err error) {
 	return t, nil
 }
 
+// WriteByte writes a single byte into the binary tree.
 func (t *binTree) WriteByte(c byte) error {
 	t.x = (t.x << 8) | uint32(c)
 	t.hoff++
@@ -57,6 +75,7 @@ func (t *binTree) WriteByte(c byte) error {
 	}
 	v := t.front
 	if int64(v) < t.hoff {
+		// We are overwriting old nodes stored in the tree.
 		t.remove(v)
 	}
 	t.node[v].x = t.x
@@ -68,6 +87,7 @@ func (t *binTree) WriteByte(c byte) error {
 	return nil
 }
 
+// Writes writes a sequence of bytes into the binTree structure.
 func (t *binTree) Write(p []byte) (n int, err error) {
 	for _, c := range p {
 		t.WriteByte(c)
@@ -75,9 +95,13 @@ func (t *binTree) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
+// add puts the node v into the tree. The node must not be part of the
+// tree before.
 func (t *binTree) add(v uint32) {
 	vn := &t.node[v]
+	// Set left and right to null indices.
 	vn.l, vn.r = null, null
+	// If the binary tree is empty make v the root.
 	if t.root == null {
 		t.root = v
 		vn.p = null
@@ -85,6 +109,7 @@ func (t *binTree) add(v uint32) {
 	}
 	x := vn.x
 	p := t.root
+	// Search for the right leave link and add the new node.
 	for {
 		pn := &t.node[p]
 		if x <= pn.x {
@@ -105,6 +130,8 @@ func (t *binTree) add(v uint32) {
 	}
 }
 
+// parent returns the parent node index of v and the pointer to v value
+// in the parent.
 func (t *binTree) parent(v uint32) (p uint32, ptr *uint32) {
 	if t.root == v {
 		return null, &t.root
@@ -118,11 +145,13 @@ func (t *binTree) parent(v uint32) (p uint32, ptr *uint32) {
 	return
 }
 
+// Remove node v.
 func (t *binTree) remove(v uint32) {
 	vn := &t.node[v]
 	p, ptr := t.parent(v)
 	l, r := vn.l, vn.r
 	if l == null {
+		// Move the right child up.
 		*ptr = r
 		if r != null {
 			t.node[r].p = p
@@ -130,16 +159,17 @@ func (t *binTree) remove(v uint32) {
 		return
 	}
 	if r == null {
+		// Move the left child up.
 		*ptr = l
 		t.node[l].p = p
 		return
 	}
 
-	// search the in-order predecessor u
+	// Search the in-order predecessor u.
 	un := &t.node[l]
 	ur := un.r
 	if ur == null {
-		// we move the left child of v, u, up
+		// In order predecessor is l. Move it up.
 		un.r = r
 		t.node[r].p = l
 		un.p = p
@@ -148,6 +178,7 @@ func (t *binTree) remove(v uint32) {
 	}
 	var u uint32
 	for {
+		// Look for the max value in the tree where l is root.
 		u = ur
 		ur = t.node[u].r
 		if ur == null {
@@ -171,27 +202,110 @@ func (t *binTree) remove(v uint32) {
 	un.p = p
 }
 
-func (t *binTree) find(x uint32) (v uint32) {
-	if t.root == null {
-		return null
+// search looks for the node that have the value x or for the nodes that
+// brace it. The node highest in the tree with the value x will be
+// returned. All other nodes with the same value live in left subtree of
+// the returned node.
+func (t *binTree) search(v uint32, x uint32) (a, b uint32) {
+	a, b = null, null
+	if v == null {
+		return
 	}
-	p := t.root
 	for {
-		pn := &t.node[p]
-		if x <= pn.x {
-			if x == pn.x || pn.l == null {
-				return p
+		vn := &t.node[v]
+		if x <= vn.x {
+			if x == vn.x {
+				return v, v
 			}
-			p = pn.l
+			b = v
+			if vn.l == null {
+				return
+			}
+			v = vn.l
 		} else {
-			if pn.r == null {
-				return p
+			a = v
+			if vn.r == null {
+				return
 			}
-			p = pn.r
+			v = vn.r
 		}
 	}
 }
 
+// max returns the node with maximum value in the subtree with v as
+// root.
+func (t *binTree) max(v uint32) uint32 {
+	if v == null {
+		return null
+	}
+	for {
+		r := t.node[v].r
+		if r == null {
+			return v
+		}
+		v = r
+	}
+}
+
+// min returns the node with the minimum value in the subtree with v as
+// root.
+func (t *binTree) min(v uint32) uint32 {
+	if v == null {
+		return null
+	}
+	for {
+		l := t.node[v].l
+		if l == null {
+			return v
+		}
+		v = l
+	}
+}
+
+// pred returns the in-order predecessor of node v.
+func (t *binTree) pred(v uint32) uint32 {
+	if v == null {
+		return null
+	}
+	u := t.max(t.node[v].l)
+	if u != null {
+		return u
+	}
+	for {
+		p := t.node[v].p
+		if p == null {
+			return null
+		}
+		if t.node[p].r == v {
+			return p
+		}
+		v = p
+	}
+}
+
+// succ returns the in-order successor of node v.
+func (t *binTree) succ(v uint32) uint32 {
+	if v == null {
+		return null
+	}
+	u := t.min(t.node[v].r)
+	if u != null {
+		return u
+	}
+	for {
+		p := t.node[v].p
+		if p == null {
+			return null
+		}
+		if t.node[p].l == v {
+			return p
+		}
+		v = p
+	}
+}
+
+// xval converts the first four bytes of a into an 32-bit unsigned
+// integer in big-endian order.
 func xval(a []byte) uint32 {
 	x := uint32(a[0]) << 24
 	x |= uint32(a[1]) << 16
@@ -200,6 +314,7 @@ func xval(a []byte) uint32 {
 	return x
 }
 
+// dumpX converts value x into a four-letter string.
 func dumpX(x uint32) string {
 	a := make([]byte, 4)
 	for i := 0; i < 4; i++ {
@@ -213,28 +328,29 @@ func dumpX(x uint32) string {
 	return string(a)
 }
 
-func writeIndent(w io.Writer, indent int) {
+// dumpNode writes a representation of the node v into the io.Writer.
+func (t *binTree) dumpNode(w io.Writer, v uint32, indent int) {
+	if v == null {
+		return
+	}
+
+	vn := &t.node[v]
+
+	t.dumpNode(w, vn.r, indent+2)
+
 	for i := 0; i < indent; i++ {
 		fmt.Fprint(w, " ")
 	}
-}
-
-func (t *binTree) dumpNode(w io.Writer, v uint32, indent int) {
-	writeIndent(w, indent)
-	if v == null {
-		fmt.Fprintln(w, "null")
-		return
-	}
-	vn := &t.node[v]
 	if vn.p == null {
 		fmt.Fprintf(w, "node %d %q parent null\n", v, dumpX(vn.x))
 	} else {
 		fmt.Fprintf(w, "node %d %q parent %d\n", v, dumpX(vn.x), vn.p)
 	}
+
 	t.dumpNode(w, vn.l, indent+2)
-	t.dumpNode(w, vn.r, indent+2)
 }
 
+// dump prints a representation of the binary tree into the writer.
 func (t *binTree) dump(w io.Writer) error {
 	bw := bufio.NewWriter(w)
 	t.dumpNode(bw, t.root, 0)
--- lzma/bintree_test.go
+++ lzma/bintree_test.go
@@ -2,7 +2,6 @@ package lzma
 
 import (
 	"io"
-	"os"
 	"testing"
 )
 
@@ -19,13 +18,39 @@ func TestBinTree_Find(t *testing.T) {
 	if n != len(s) {
 		t.Fatalf("WriteString returned %d; want %d", n, len(s))
 	}
+
+	/* dump info writes the complete tree
 	if err = bt.dump(os.Stdout); err != nil {
 		t.Fatalf("bt.dump error %s", err)
 	}
+	*/
+
 	tests := []string{"Sieg", "Sieb", "Simu"}
 	for _, c := range tests {
 		x := xval([]byte(c))
-		v := bt.find(x)
-		t.Logf("v for %q: %d", c, v)
+		a, b := bt.search(bt.root, x)
+		t.Logf("%q: a, b == %d, %d", c, a, b)
+	}
+}
+
+func TestBinTree_PredSucc(t *testing.T) {
+	bt, err := newBinTree(30)
+	if err != nil {
+		t.Fatal(err)
+	}
+	const s = "Klopp feiert mit Liverpool seinen hoechsten Sieg."
+	n, err := io.WriteString(bt, s)
+	if err != nil {
+		t.Fatalf("WriteString error %s", err)
+	}
+	if n != len(s) {
+		t.Fatalf("WriteString returned %d; want %d", n, len(s))
+	}
+	for v := bt.min(bt.root); v != null; v = bt.succ(v) {
+		t.Log(dumpX(bt.node[v].x))
+	}
+	t.Log("")
+	for v := bt.max(bt.root); v != null; v = bt.pred(v) {
+		t.Log(dumpX(bt.node[v].x))
 	}
 }
