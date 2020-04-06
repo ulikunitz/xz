db43713aaf766ca7ac9f2cfb70441db55b3a530d
Frederick Robinson <frederickrobinson@penguin>
2020-04-05 20:22:32 -0700

    add chunked test file

--- reader_at_test.go
+++ reader_at_test.go
@@ -10,17 +10,18 @@ import (
 func TestReaderAtSimple(t *testing.T) {
 	testFile(t, "testfiles/fox.xz")
 	testFile(t, "testfiles/fox-check-none.xz")
+	testFile(t, "testfiles/fox.blocks.xz")
 }
 
-func testFile(t *testing.T, file string) {
-	xz, err := os.Open(file)
+func testFile(t *testing.T, filePath string) {
+	xz, err := os.Open(filePath)
 	if err != nil {
-		t.Fatalf("os.Open(%q) error %s", file, err)
+		t.Fatalf("os.Open(%q) error %s", filePath, err)
 	}
 
-	info, err := os.Stat(file)
+	info, err := os.Stat(filePath)
 	if err != nil {
-		t.Fatalf("os.Stat(%q) error %s", file, err)
+		t.Fatalf("os.Stat(%q) error %s", filePath, err)
 	}
 
 	conf := ReaderAtConfig{
@@ -37,7 +38,7 @@ func testFile(t *testing.T, file string) {
 	}
 
 	bufStr := buf.String()
-	expected := "The quick brown fox jumps over the lazy dog.\n" // fixme
+	expected := "The quick brown fox jumps over the lazy dog.\n"
 	if bufStr != expected {
 		t.Fatalf("Unexpected decompression output. \"%s\" != \"%s\"", bufStr, expected)
 	}
