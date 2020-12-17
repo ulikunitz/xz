f84da4cdfa12a40496c405b145e6216d90bd0e01
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2020-12-17 20:02:14 +0100

    lzma: fix all ctaticcheck issues.
    This commit fixes all staticcheck issues. It mainly is a removal of
    unused functions but includes an additional const type issue.

--- lzma/decoderdict_test.go
+++ lzma/decoderdict_test.go
@@ -5,23 +5,8 @@
 package lzma
 
 import (
-	"fmt"
 	"testing"
 )
-
-func peek(d *decoderDict) []byte {
-	p := make([]byte, d.buffered())
-	k, err := d.peek(p)
-	if err != nil {
-		panic(fmt.Errorf("peek: "+
-			"Read returned unexpected error %s", err))
-	}
-	if k != len(p) {
-		panic(fmt.Errorf("peek: "+
-			"Read returned %d; wanted %d", k, len(p)))
-	}
-	return p
-}
 
 func TestNewDecoderDict(t *testing.T) {
 	if _, err := newDecoderDict(0); err == nil {
