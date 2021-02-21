d2624c667fd52b03d5f2b1d996c68f6d81b50ae2
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2021-02-21 11:41:25 +0100

    Add example_test.go
    Issue #43 asks for simple examples for reading and writing files. These
    examples have been added to example_test.go.

---
+++ example_test.go
@@ -0,0 +1,49 @@
+package xz_test
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/ulikunitz/xz"
+)
+
+func ExampleReader() {
+	f, err := os.Open("fox.xz")
+	if err != nil {
+		log.Fatalf("os.Open(%q) error %s", "fox.xz", err)
+	}
+	defer f.Close()
+	r, err := xz.NewReader(bufio.NewReader(f))
+	if err != nil {
+		log.Fatalf("xz.NewReader(f) error %s", err)
+	}
+	if _, err = io.Copy(os.Stdout, r); err != nil {
+		log.Fatalf("io.Copy error %s", err)
+	}
+	// Output:
+	// The quick brown fox jumps over the lazy dog.
+}
+
+func ExampleWriter() {
+	f, err := os.Create("example.xz")
+	if err != nil {
+		log.Fatalf("os.Open(%q) error %s", "example.xz", err)
+	}
+	defer f.Close()
+	w, err := xz.NewWriter(f)
+	if err != nil {
+		log.Fatalf("xz.NewWriter(f) error %s", err)
+	}
+	defer w.Close()
+	_, err = fmt.Fprintln(w, "The brown fox jumps over the lazy dog.")
+	if err != nil {
+		log.Fatalf("fmt.Fprintln error %s", err)
+	}
+	if err = w.Close(); err != nil {
+		log.Fatalf("w.Close() error %s", err)
+	}
+	// Output:
+}
