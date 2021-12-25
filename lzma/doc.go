bac0399bfbe28b1ecfe455fdae560da91b4cfbce
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2021-12-25 10:43:48 +0100

    lzma: improve package documentation
    Expressions in the lzma package documentation have been changed for more
    clarity.

--- lzma/doc.go
+++ lzma/doc.go
@@ -1,4 +1,4 @@
-// Package lzma procides capabilities to compress into decompress from LZMA and
-// LZMA2 streams. The LZMA stream is uninterrupted range encoded stream and
-// LZMA2 splits the data into chunks to allow parallel generation and reading.
+// Package lzma procides capabilities to compress into and decompress from LZMA and
+// LZMA2 streams. The LZMA stream is uninterrupted range encoded stream. LZMA2
+// is a series of chunks of individually range-encoded streams.
 package lzma
