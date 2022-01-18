a8b25b806e466b932c8e92960520d5c8cd6912d2
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2022-01-18 22:30:39 +0100

    lzma_1: remove package
    The progress with lzma allows us to remove lzma_1.

--- lzma_1/doc.go
+++
@@ -1,4 +0,0 @@
-// package lzma_1 procides capabilities to compress into and decompress from LZMA and
-// LZMA2 streams. The LZMA stream is uninterrupted range encoded stream. LZMA2
-// is a series of chunks of individually range-encoded streams.
-package lzma_1
