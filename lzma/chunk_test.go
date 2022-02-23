4bd638fb5c8f1127b8642140fa45d43af54fd2e6
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2022-02-23 21:10:59 +0100

    lzma: added test for chunkWriter.Close

--- lzma/chunk_test.go
+++ lzma/chunk_test.go
@@ -182,3 +182,39 @@ func TestChunkWriterReader(t *testing.T) {
 		})
 	}
 }
+
+func TestChunkClose(t *testing.T) {
+	const s = "=====foofoobar==foobar===="
+
+	var cw chunkWriter
+	buf := new(bytes.Buffer)
+	var lzCfg = lz.Config{}
+	lzCfg.ApplyDefaults()
+	seq, err := lzCfg.NewSequencer()
+	if err != nil {
+		t.Fatalf("lzcfg.NewSequencer() error %s", err)
+	}
+	if err = cw.init(buf, seq, []byte(s), Properties{3, 0, 2}); err != nil {
+		t.Fatalf("cw.init() error %s", err)
+	}
+	if err = cw.Close(); err != nil {
+		t.Fatalf("cw.Close() error %s", err)
+	}
+
+	var cr chunkReader
+	dictSize := seq.WindowPtr().WindowSize
+	if err = cr.init(buf, dictSize); err != nil {
+		t.Fatalf("cr.init() error %s", err)
+	}
+
+	out := new(bytes.Buffer)
+	if _, err = io.Copy(out, &cr); err != nil {
+		t.Logf("out %q", out.String())
+		t.Fatalf("io.Copy(out, cr) error %s", err)
+	}
+	g := out.String()
+
+	if g != s {
+		t.Fatalf("got %q; want %q", g, s)
+	}
+}
