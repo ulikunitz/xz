c622241af031cf8ed492a0ce71dc3f2c75a8c5d5
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2023-04-30 10:48:16 +0200

    Added a fuzz test to xz
    This fuzz test is for checking whether everything works as expected.

--- xz_test.go
+++ xz_test.go
@@ -6,6 +6,7 @@ package xz_test
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"io"
 	"testing"
 
@@ -26,4 +27,70 @@ func TestPanic(t *testing.T) {
 		t.Logf("ioutil.ReadAll(r) error %s", err)
 		return
 	}
+}
+
+func FuzzXZ(f *testing.F) {
+	f.Add(1, []byte(""))
+	f.Add(3, []byte(""))
+	const foobar = "====foofoobarfoobar tender==="
+	f.Add(1, []byte(foobar))
+	f.Add(10, []byte(foobar))
+	f.Fuzz(func(t *testing.T, workers int, data []byte) {
+		if !(0 <= workers && workers <= 32) {
+			t.Skip()
+		}
+		wc := xz.WriterConfig{Workers: workers}
+		wc.ApplyDefaults()
+		var err error
+		if err = wc.Verify(); err != nil {
+			t.Skip()
+		}
+		h1 := sha256.New()
+		var buf bytes.Buffer
+		w, err := xz.NewWriterConfig(&buf, wc)
+		if err != nil {
+			t.Fatalf("NewWriterConfig(&buf, %+v) error %s", wc, err)
+		}
+		defer w.Close()
+		mw := io.MultiWriter(w, h1)
+		n, err := mw.Write(data)
+		if err != nil {
+			t.Fatalf("w.Write(data) error %s", err)
+		}
+		if n != len(data) {
+			t.Fatalf("w.Write(data) got n=%d; want %d",
+				n, len(data))
+		}
+		if err = w.Close(); err != nil {
+			t.Fatalf("w.Close() error %s", err)
+		}
+		h2 := sha256.New()
+		rc := xz.ReaderConfig{Workers: workers}
+		rc.ApplyDefaults()
+		if err = rc.Verify(); err != nil {
+			t.Fatalf("rc.Verify() for %+v error %s", rc, err)
+		}
+		r, err := xz.NewReaderConfig(&buf, rc)
+		if err != nil {
+			t.Fatalf("xz.NewReaderConfig(&buf, %+v) error %s",
+				rc, err)
+		}
+		defer r.Close()
+		k, err := io.Copy(h2, r)
+		if err != nil {
+			t.Fatalf("io.Copy(h2, r) error %s", err)
+		}
+		if k != int64(len(data)) {
+			t.Fatalf("io.Copy(h2, r) returned %d; want %d",
+				k, len(data))
+		}
+		h1sum := h1.Sum(nil)
+		h2sum := h2.Sum(nil)
+		if !bytes.Equal(h1sum, h2sum) {
+			t.Fatalf("hash sums differ")
+		}
+		if err = r.Close(); err != nil {
+			t.Fatalf("r.Close() error %s", err)
+		}
+	})
 }
