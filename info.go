3e444e37331d533911d392188594e2dd8033cdd4
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2024-04-22 08:36:47 +0200

    xz: work on info.go
    Introduce streamWalkParser to group the parsing of the stream.

--- info.go
+++ info.go
@@ -4,39 +4,12 @@ import (
 	"errors"
 	"io"
 
+	"github.com/ulikunitz/xz/internal/discard"
 	"github.com/ulikunitz/xz/lzma"
 )
 
-// skip skips over the next n bytes in the reader.
-func skip(r io.Reader, n int64) error {
-	if n < 0 {
-		panic("n < 0")
-	}
-
-	if s, ok := r.(io.Seeker); ok {
-		_, err := s.Seek(n, io.SeekCurrent)
-		if err == nil {
-			return nil
-		}
-	}
-
-	p := make([]byte, 16*1024)
-	for n > 0 {
-		if n < int64(len(p)) {
-			p = p[:n]
-		}
-		k, err := r.Read(p)
-		n -= int64(k)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-// If the no information about chunks should be provided the blockHeader needs
-// to provide the error message.
+// errSuppressChunks should be returned from the blockHeader callback in the
+// [walker] interface to suppress the chunkHeader callback.
 var errSuppressChunks = errors.New("suppress info on chunks")
 
 // A walker is used to visit all important parts of the XZ stream. It cannot
@@ -50,11 +23,69 @@ type walker interface {
 	record(r record) error
 }
 
+type streamWalkParser struct {
+	w  walker
+	dr discard.Reader
+	h  header
+}
+
+func newStreamWalkParser(r io.Reader, w walker, h header) *streamWalkParser {
+	return &streamWalkParser{
+		w:  w,
+		dr: discard.Wrap(r),
+		h:  h,
+	}
+}
+
+func (p *streamWalkParser) Tail() error {
+	if err := p.w.streamHeader(p.h); err != nil {
+		return err
+	}
+	for {
+		// find blocks
+		panic("TODO")
+	}
+}
+
+
+func walkStream(r io.Reader, w walker, flags byte) error {
+	h, err := readHeader(r, flags)
+	if err != nil {
+		return err
+	}
+	p := newStreamWalkParser(r, w, h)
+	return p.Tail()
+}
+
+// Flags for the walk function.
 const (
 	singleStream = 1 << iota
 )
 
 func walk(r io.Reader, w walker, flags byte) error {
+	var err error
+	if flags&singleStream != 0 {
+		if err = walkStream(r, w, 0); err != nil {
+			return err
+		}
+		// check for EOF
+		var a [1]byte
+		if _, err = r.Read(a[:]); err != io.EOF {
+			return errors.New(
+				"xz: expected EOF at end of single stream")
+		}
+		return nil
+	}
 
-	return errors.ErrUnsupported
+	if err = walkStream(r, w, 0); err != nil {
+		return err
+	}
+	for {
+		if err = walkStream(r, w, expectPadding); err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+	}
 }
