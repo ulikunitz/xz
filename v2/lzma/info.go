e4e13f90de2601ac0d623271f7f78f858dece01d
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2026-05-25 19:44:04 +0200

    lzma: add info functions

---
+++ v2/lzma/info.go
@@ -0,0 +1,66 @@
+package lzma
+
+import (
+	"errors"
+	"io"
+	"math"
+
+	"github.com/ulikunitz/xz/v2/internal/stream"
+)
+
+// Stat provides the size of the uncompressed file as well as the compressed
+// size of an lzma file. The function doesn't check for decompression errors.
+func Stat(r io.Reader) (info Info, err error) {
+	z := stream.Wrap(r)
+	start := z.Offset()
+
+	var p = make([]byte, headerLen)
+	if _, err = io.ReadFull(z, p); err != nil {
+		return info, err
+	}
+	var hdr Header
+	if err = hdr.UnmarshalBinary(p); err != nil {
+		return info, err
+	}
+	if err = hdr.verify(); err != nil {
+		return info, err
+	}
+
+	switch {
+	case hdr.uncompressedSize == EOSSize:
+		info.Uncompressed = -1
+	case hdr.uncompressedSize <= math.MaxInt64:
+		info.Uncompressed = int64(hdr.uncompressedSize)
+	default:
+		return info, errors.New("lzma: size overflow")
+	}
+
+	if info.Uncompressed >= 0 {
+		if s, ok := r.(io.Seeker); ok {
+			end, err := s.Seek(0, io.SeekEnd)
+			if err == nil {
+				info.Compressed = end - start
+				return info, nil
+			}
+		}
+	}
+
+	if uint64(hdr.DictSize) > math.MaxInt {
+		return info, errors.New("lzma: dictSize too large")
+	}
+
+	rr := new(Reader)
+	err = rr.init(z, hdr)
+	if err != nil {
+		return info, err
+	}
+
+	n, err := io.Copy(io.Discard, rr)
+	info.Compressed = z.Offset() - start
+	if info.Uncompressed < 0 {
+		info.Uncompressed = n
+	} else if n != info.Uncompressed {
+		return info, errors.New("lzma: uncompressed size mismatch")
+	}
+	return info, err
+}
