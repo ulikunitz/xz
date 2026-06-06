85b4a60c3e4ccc4a4d8f05518167c2bfc0b877fe
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2026-06-06 11:53:15 +0200

    Make v2 clean again
    We are going back to config structs. That will require a new start.

--- v2/lzma/doc.go
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: © 2014 Ulrich Kunitz
-//
-// SPDX-License-Identifier: BSD-3-Clause
-
-// Package lzma provides support for the encoding and decoding LZMA, LZMA2 and
-// raw LZMA streams without a header.
-package lzma
